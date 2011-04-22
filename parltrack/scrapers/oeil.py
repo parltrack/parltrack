469863d22361044e84f85d01e91e9c2c3523afdb
stef <stefan.marsiske@gmail.com>
2011-04-23 00:35:45 +0200

    [enh] adapted to new schema

--- parltrack/scrapers/oeil.py
+++ parltrack/scrapers/oeil.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #    This file is part of le(n)x.
 
 #    le(n)x is free software: you can redistribute it and/or modify
@@ -30,34 +31,30 @@ from ctypes import c_int
 import urllib2, urllib, cookielib, tidy, datetime, sys, json, pymongo, traceback
 from logging import DEBUG
 
-def diff(data, param):
-    if not param: param=[0,0]
-    k=data['source']
-    if not 'Identification' in data:
-        logger.error('[!] ERROR no title in: %s\nSkipping' % k)
-        return param
+def save(data):
+    pprint.pprint(data)
+    src=data['meta']['source']
+    return
 
-    res=docs.find_one({ 'source' : k }) or {}
-    d=diffItem(dict([(k,v) for k,v in data.items() if not k in ['_id','source', 'updated', 'changes','__stage_order','__summaries_order']]),
-               dict([(k,v) for k,v in res.items() if not k in ['_id','source', 'updated', 'changes','__stage_order','__summaries_order']]))
+    res=docs.find_one({ 'source' : src }) or {}
+    d=diff(dict([(k,v) for k,v in data.items() if not k in ['_id', 'meta', 'changes',]]),
+           dict([(k,v) for k,v in res.items() if not k in ['_id', 'meta', 'changes']]))
     if d:
         if not res:
-            logger.info('adding '+ data['Identification']['Title'])
-            param[0]+=1
+            logger.info('adding '+ data['procedure']['title'])
+            stats[0]+=1
         else:
-            logger.info( 'updating '+ data['Identification']['Title'])
-            param[1]+=1
+            logger.info( 'updating '+ data['procedure']['title'])
+            stats[1]+=1
             data['_id']=res['_id']
         logger.info(d)
-        now=datetime.datetime.utcnow().replace(microsecond=0).isoformat()
-        data['updated']=now
         data['changes']=res.get('changes',{})
         data['changes'][now]=d
         docs.save(data)
-        return param
-    return param
+        return stats
+    return stats
 
-def diffItem(e1,e2):
+def diff(e1,e2):
     if type(e1) == str:
        e1=unicode(e1,'utf8')
     if type(e1) == tuple:
@@ -65,12 +62,12 @@ def diffItem(e1,e2):
     if type(e1) != type(e2):
         return {'new': e1, 'old': e2}
     if type(e1) == list:
-        return filter(None,[diffItem(*k) for k in izip_longest(sorted(e1),sorted(e2))])
+        return filter(None,[diff(*k) for k in izip_longest(sorted(e1),sorted(e2))])
     if type(e1) == dict:
         res=[]
         for k in set(e1.keys() + e2.keys()):
             if k == '_id': continue
-            r=diffItem(e1.get(k),e2.get(k))
+            r=diff(e1.get(k),e2.get(k))
             if r:
                 res.append((k,r))
         return dict(res)
@@ -94,27 +91,28 @@ def fetch(url):
     return parse(StringIO(str(raw)))
 
 def toDate(node):
-    #text=tostring(node,method="text",encoding='utf8').replace('\xc2\xa0',' ').strip()
-    text=tostring(node,method="text",encoding=unicode).replace(u"\u00A0",' ').strip().encode('raw_unicode_escape').decode('utf-8')
+    text=node.xpath("string()").replace(u"\u00A0",' ').strip()
     if text is None or not len(text): return
     lines=text.split('\n')
     if len(lines)>1:
         result=[]
         for text in lines:
-            value=[int(x) for x in text.strip().split('/')]
-            result.append(datetime.date(value[2], value[1], value[0]).toordinal())
+            if not len(text.strip()): continue
+            value=[int(x) for x in text.strip().split('/') if len(x)]
+            result.append(datetime.date(value[2], value[1], value[0]))
         return result
     else:
-        value=[int(x) for x in text.strip().split('/')]
-        return datetime.date(value[2], value[1], value[0]).toordinal()
+        if not len(text.strip()): return None
+        value=[int(x) for x in text.strip().split('/') if len(x)]
+        return datetime.date(value[2], value[1], value[0])
 
 def toText(node):
     if node is None: return ''
-    text=tostring(node,method="text",encoding=unicode).replace(u"\u00A0",' ').strip().encode('raw_unicode_escape').decode('utf-8')
+    text=node.xpath("string()").replace(u"\u00A0",' ').strip()
 
     links=node.xpath('a')
     if not links: return text
-    return {'Title': text, 'URL': unicode(urljoin(base,links[0].get('href')),'utf8')}
+    return {'title': text, 'url': unicode(urljoin(base,links[0].get('href')),'utf8')}
 
 def toLines(node):
     text=toText(node).split('\n')
@@ -135,7 +133,7 @@ def convertRow(cells,fields):
     if not len(cells)==len(fields): return None
     for i,cell in enumerate(cells):
         tmp=fields[i][1](cell)
-        if tmp: res[fields[i][0]]=tmp
+        if tmp: res[fields[i][0].lower().replace(' ', '_')]=tmp
     return res
 
 def toObj(table,fields):
@@ -162,23 +160,22 @@ def identification(table):
     res={}
     for row in table.xpath('tr'):
         items=row.xpath('td')
-        #key=tostring(items[0],method="text",encoding='utf8').strip()
-        key=tostring(items[0],method="text",encoding=unicode).replace(u"\u00A0",' ').strip().encode('raw_unicode_escape').decode('utf-8')
+        key=items[0].xpath("string()").replace(u"\u00A0",' ').strip()
         if key=='Subject(s)':
-            value=[x.replace('\xc2\xa0',' ').strip() for x in items[1].xpath('text()')]
+            value=[x.replace(u'\u00a0',' ').strip() for x in items[1].xpath('text()')]
         else:
-            #value=tostring(items[1],method="text",encoding='utf8').replace('\xc2\xa0',' ').strip()
-            value=tostring(items[1],method="text",encoding=unicode).replace(u"\u00A0",' ').strip().encode('raw_unicode_escape').decode('utf-8')
+            value=items[1].xpath("string()").replace(u"\u00A0",' ').strip()
+        if key.lower()==u'legal basis':
+            value=[x.strip() for x in value.split(';')]
         if key and value:
-            res[key]=value
+            res[str(key).lower().replace(' ','_')]=value
     return res
 
 def forecasts(table):
     res={}
     for row in table.xpath('tr'):
         items=row.xpath('td')
-        #key=tostring(items[2],method="text",encoding='utf8').replace('\xc2\xa0',' ').strip()
-        key=tostring(items[2],method="text",encoding=unicode).replace(u"\u00A0",' ').strip().encode('raw_unicode_escape').decode('utf-8')
+        key=items[2].xpath("string()").replace(u"\u00A0",' ').strip()
         date=toDate(items[0])
         if key and date:
             res[key]=date
@@ -188,10 +185,8 @@ def links(table):
     res={}
     for row in table.xpath('tr'):
         items=row.xpath('td')
-        #key=unicode(tostring(items[0],method="text",encoding='utf8').strip(),'utf8')
-        key=tostring(items[0],method="text",encoding=unicode).replace(u"\u00A0",' ').strip().encode('raw_unicode_escape').decode('utf-8')
-        #value=unicode(tostring(items[1],method="text",encoding='utf8').strip(),'utf8')
-        value=tostring(items[1],method="text",encoding=unicode).replace(u"\u00A0",' ').strip().encode('raw_unicode_escape').decode('utf-8')
+        key=items[0].xpath("string()").replace(u"\u00A0",' ').strip()
+        value=items[1].xpath("string()") .replace(u"\u00A0",' ').strip()
         url=items[1].xpath('a')[0].get('href')
         if key and value:
             res[key]={'Title': value, 'URL': url}
@@ -200,38 +195,44 @@ def links(table):
     return res
 
 def agents(table):
-    res={}
     tmp=toObj(table,agentFields)
+    res=[]
     for row in tmp:
-        if 'Rapporteur' in row:
-            # convert Rapporteurs to an own dict
-            if not (type(row['Appointed'])==type(row['Rapporteur'])==type(row['Political Group'])==list):
-                meps=[{'Rapporteur': row['Rapporteur'],
-                      'Appointed': row['Appointed'],
-                      'Group': row['Political Group']}]
-            else:
-                meps=[{'Rapporteur': p[0], 'Appointed': p[1], 'Group': p[2]}
-                      for p in zip(row['Rapporteur'],row['Appointed'],row['Political Group'])]
-            row['Rapporteurs']={}
-            for mep in meps:
-                row['Rapporteurs'][mep['Rapporteur'].replace('.','')]={'Date': mep['Appointed'], 'Group': mep['Group']}
-            del(row['Rapporteur'])
-            del(row['Appointed'])
-            del(row['Political Group'])
-
-        # make the whole commitee a dict as well
-        commitee=row['Commitee']
+        commitee=row['commitee']
         if commitee:
-            del(row['Commitee'])
             tmp1=commitee.split('(')
             commitee=tmp1[0].strip()
-            row['role']=tmp1[1].strip()[:-1]
-            if commitee in res:
-                logger.error('duplicate agents: %s' % commitee)
-                #raise ValueError('duplicate agents: %s' % commitee)
-                raise ValueError
-            res[commitee]=row
-    OtherAgents(table, res)
+            comrole=tmp1[1].strip()[:-1]
+            if not comrole in ['responsible', 'opinion']:
+                print comrole
+                comrole=''
+            elif comrole.startswith('responsible'):
+                comrole=True
+            else:
+                comrole=False
+        if 'Rapporteur' in row:
+            # convert Rapporteurs to an own dict
+            if not (type(row.get('Appointed'))==type(row.get('Rapporteur'))==type(row.get('Political Group'))==list):
+                agent={}
+                agent['commitee']=row['Commitee']
+                agent['responsible']=comrole
+                agent['name']=row.get('Rapporteur')
+                agent['function']='MEP'
+                agent['appointed']=row.get('Appointed')
+                agent['group']=row.get('Political Group')
+                res.append(agent)
+            else:
+                res.append([{'commitee': row['Commitee'],
+                            'responsible': comrole,
+                            'name': p[0],
+                            'function': 'MEP',
+                            'appointed': p[1],
+                            'group': p[2]}
+                           for p in zip(row['Rapporteur'],
+                                        row['Appointed'],
+                                        row['Political Group'])])
+        # make the whole commitee a dict as well
+    OtherAgents(table,res)
     return res
 
 def OtherAgents(table,res):
@@ -243,43 +244,31 @@ def OtherAgents(table,res):
     for row in table.xpath('tr'):
         fields=row.xpath('td')
         if(len(fields)==3 and toText(fields[0]).startswith('European Commission DG')):
-            item={'Agent': toText(fields[0]), }
-            key=toText(fields[1])
+            agent={'institution': toText(fields[0]),
+                   'department': toText(fields[1])}
             tmp=toText(fields[2]).split(' ')
             if len(tmp)==3:
                 value=[int(x) for x in tmp[2].strip().split('/')]
-                item['Date']=datetime.date(value[2], value[1], value[0]).toordinal()
-            res[key]=item
+                agent['submission_date']=datetime.date(value[2], value[1], value[0])
+            else:
+                print 'other agents, non-date:', tmp
+            res.append(agent)
         elif(len(fields)==5 and toText(fields[0]).startswith('Council of the Union')):
-            item={'Agent': 'Council of the Union',
-                  'URL': urlFromJS(fields[1]),}
-            key=toText(fields[2])
+            agent={'institution': toText(fields[0]),
+                  'department': toText(fields[2]),}
+            tmp=urlFromJS(fields[1])
+            if tmp: agent['council_doc']=tmp
             tmp=toText(fields[3]).split(' ')
             if len(tmp)==2:
-                item['Meeting number']=tmp[1]
+                agent['meeting_id']=tmp[1]
             tmp=toText(fields[4]).split(' ')
             if len(tmp)==2:
                 value=[int(x) for x in tmp[1].strip().split('/')]
-                item['Date']=datetime.date(value[2], value[1], value[0]).toordinal()
-            res[key]=item
+                agent['meeting_date']=datetime.date(value[2], value[1], value[0])
+            res.append(agent)
         else:
             raise ValueError
             logger.error('unparsed row: %s %s' % (len(fields), toText(row)))
-
-def stages(table):
-    tmp=toObj(table,stageFields)
-    res={}
-    order=[]
-    for stage in tmp:
-        if not 'stage' in stage:
-            raise ValueError, 'no stage' % stage
-        key=stage['stage']
-        if key in res:
-            raise ValueError, 'duplicate stage' % key
-        order.append(key)
-        del(stage['stage'])
-        res[key]=stage
-    return (res, order)
 
 def summaries(table):
     tmp=toObj(table,summaryFields)
@@ -296,15 +285,15 @@ def summaries(table):
         res[key]=summary
     return (res, order)
 
-stageFields=( ('stage', toText),
+stageFields=( ('title', toText),
               ('stage document',urlFromJS),
-              ('source institution',toText),
+              ('body',toText),
               ('source reference',toText),
-              ('Equivalent references', toText),
+              ('equivalent references', toText),
               ('Vote references', toText),
-              ('Amendment references', urlFromJS),
+              ('amendment references', urlFromJS),
               ('Joint Resolution', toText),
-              ('Date of document', toDate),
+              ('date', toDate),
               ('Date of publication in Official Journal', urlFromJS)
             )
 agentFields=( ('Commitee', toText),
@@ -328,24 +317,40 @@ def scrape(url):
         # even retry failed :(
         logger.error('[!] ERROR no title in: %s\nSkipping' % k)
         return
-    res={'source': url}
+    res={'meta': {'source': url,
+                  'country': 'eu',
+                  'updated': datetime.datetime.utcnow() } }
     for section in sections:
         table=section.xpath('../../../following-sibling::*')[0]
         if section.text in ['Identification procedure', 'Identification', 'Identification resolution', 'Identification document']:
-            res['Identification']=identification(table)
+            res['procedure']=identification(table)
         elif section.text in ['Stages procedure', 'Stages', 'Stages resolution']:
-            res['Stages'],res['__stage_order']=stages(table)
+            tmp=toObj(table,stageFields)
+            res['stages']=[]
+            for doc in tmp:
+                if 'source_reference' in doc:
+                    if type(doc['source_reference'])==dict:
+                        doc['source']=doc['source_reference']['title']
+                        doc['source_link']=doc['source_reference']['url']
+                    else:
+                        doc['source']=doc['source_reference']
+                    del(doc['source_reference'])
+                if 'equivalent_references' in doc:
+                    doc['documents']=[doc.get('equivalent_references')]
+                    del(doc['equivalent_references'])
+                res['stages'].append(doc)
         elif section.text == 'Forecasts procedure':
-            res['Forecasts']=forecasts(table)
+            res['procedure']['forecasts']=forecasts(table)
         elif section.text in ['Agents procedure', 'Agents', 'Agents document', 'Agents resolution']:
-            res['Agents']=agents(table)
+            res['procedure']['actors']=agents(table)
         elif section.text == 'Links to other sources procedure':
-            res['Links to other sources procedure']=links(table)
+            res['procedure']['links']=links(table)
         elif section.text == 'List of summaries':
-            res['List of summaries'],res['__summaries_order']=summaries(table)
+            #res['List of summaries'],res['__summaries_order']=summaries(table)
+            pass
         else:
             logger.warning('[*] unparsed: '+ section.text)
-    return res
+    save(res)
 
 def getStages():
     tree=fetch('http://www.europarl.europa.eu/oeil/search_procstage_stage.jsp')
@@ -366,7 +371,7 @@ def nextPage(req):
                             'doctype' : "strict",
                             'wrap' : 0})
     tree=parse(StringIO(str(raw)))
-    map(m.addjob, ['http://www.europarl.europa.eu/oeil/'+x.get('href')
+    map(scrape, ['http://www.europarl.europa.eu/oeil/'+x.get('href')
                    for x
                    in tree.xpath('//a[@class="com_acronym"]')])
 
@@ -395,61 +400,24 @@ def crawl(fast=True):
         #result.extend(nextPage(req))
     return result
 
-class Multiplexer(object):
-    def __init__(self, worker, writer, threads=8):
-        self.worker=worker
-        self.writer=writer
-        self.consumer=Process(target=self.consume)
-        self.pool = Pool(threads)   # reduce if less aggressive
-        self.consume = Value(c_bool,True)
-        self.q=Queue()
-
-    def start(self):
-        self.consume.value=True
-        self.consumer.start()
-
-    def addjob(self, url):
-        try:
-            self.pool.apply_async(self.worker,[url],callback=self.q.put)
-        except:
-            logger.error('[!] failed to scrape '+ url)
-            traceback.print_exc(file=sys.stderr)
-            #raise
-
-    def finish(self):
-        self.pool.close()
-        self.pool.join()
-        self.consume.value=False
-        self.consumer.join()
-
-    def consume(self):
-        param=None
-        while True:
-            try:
-                param = self.writer(self.q.get(True, timeout=1), param)
-            except Empty:
-                if not self.consume.value: break
-        logger.info('result: %s' % param)
-
 # and some global objects
 base = 'http://www.europarl.europa.eu/oeil/file.jsp'
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
 opener.addheaders = [('User-agent', 'weurstchen/0.5')]
 # connect to  mongo
 conn = pymongo.Connection()
-db=conn.oeil
-docs=db.docs
+db=conn.parltrack
+procedures=db.procedures
 logger = log_to_stderr()
 logger.setLevel(DEBUG)
+stats=[0,0]
 
 if __name__ == "__main__":
-    m=Multiplexer(scrape,diff)
-    m.start()
-    crawl(fast=(False if len(sys.argv)>1 and sys.argv[1]=='full' else True))
-    m.finish()
+    import pprint
+    scrape("http://www.europarl.europa.eu/oeil/file.jsp?id=5563972")
+    #crawl(fast=(False if len(sys.argv)>1 and sys.argv[1]=='full' else True))
 
     # some tests
-    #import pprint
     #scrape("http://www.europarl.europa.eu/oeil/file.jsp?id=5696252")
     #scrape("http://www.europarl.europa.eu/oeil/file.jsp?id=5872922")
     #pprint.pprint(scrape("http://www.europarl.europa.eu/oeil/file.jsp?id=5872922"))
