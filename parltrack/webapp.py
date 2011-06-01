e2c39c513b9bc1db09ebdeab1d04f1219bfae7f5
asciimoo <asciimoo@gmail.com>
2011-06-01 20:08:23 +0200

    [doc] file header added

--- parltrack/webapp.py
+++ parltrack/webapp.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+#    This file is part of parltrack.
+
+#    parltrack is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    parltrack is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with parltrack.  If not, see <http://www.gnu.org/licenses/>.
+
+# (C) 2011 by Adam Tauber, <asciimoo@gmail.com>
+
 import os
 from pymongo import Connection
 from flaskext.mail import Mail
