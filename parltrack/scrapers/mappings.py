68317c759cab0f591e626b9832932e2f1466d070
stef <stefan.marsiske@gmail.com>
2011-06-06 18:10:47 +0200

    [mod] refactored mapping into own file

---
+++ parltrack/scrapers/mappings.py
@@ -0,0 +1,26 @@
+
+
+ALL_STAGES= [ "Preparatory phase in Parliament",
+              "Awaiting Parliament 1st reading / single reading / budget 1st stage",
+              "Awaiting reconsultation",
+              "Awaiting Council decision, blocked at 1st reading",
+              "Awaiting Council 1st reading position / budgetary conciliation convocation",
+              "Budgetary conciliation committee convened",
+              "Awaiting announcement of budgetary joint text",
+              "Awaiting budgetary conciliation report",
+              "Awaiting Parliament decision on budgetary joint text",
+              "Awaiting Council decision on budgetary joint text",
+              "Awaiting Parliament decision after Council rejection of joint text",
+              "Awaiting Parliament 2nd reading",
+              "Awaiting Council decision, 2nd reading",
+              "Conciliation ongoing",
+              "Conciliation ended",
+              "Awaiting Parliament and Council decision, 3rd reading",
+              "Political agreement on final act",
+              "Awaiting signature",
+              "Awaiting final decision",
+              "Procedure completed, awaiting publication in Official Journal",
+              "Procedure completed",
+              "Procedure rejected",
+              "Procedure lapsed or withdrawn"]
+
