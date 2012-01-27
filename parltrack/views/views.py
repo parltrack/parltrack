9f0f20ff596b7d617c3ef53ea5a1911371de1832
stef <stefan.marsiske@gmail.com>
2012-01-27 21:37:34 +0100

    [fix] added vp to com positions #v6

--- parltrack/views/views.py
+++ parltrack/views/views.py
@@ -34,6 +34,7 @@ group_positions={u'Chair': 10,
                  u'Member': 0,
                  }
 com_positions={"Chair": 4,
+               "Vice-President": 3,
                "Vice-Chair": 3,
                "Member": 2,
                "Substitute": 1,
