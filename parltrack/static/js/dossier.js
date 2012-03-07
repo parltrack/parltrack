703ec11397e0a54f1d4bc5abeabce03bc1832398
stef <stefan.marsiske@gmail.com>
2012-03-07 23:21:42 +0100

    [enh] epheader added for dossiers

--- parltrack/static/js/dossier.js
+++ parltrack/static/js/dossier.js
@@ -1,6 +1,40 @@
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function deleteCookie(name) {
+    setCookie(name,"",-1);
+}
+function setCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
 $(document).ready(function() {
   $('.button').click(function() {
    $(this).next().toggleClass('hidden');
+  });
+  if(getCookie('epheader')) {
+   $('#epheader').removeClass('hidden');
+  }
+  $('#toggle_ep').click(function() {
+   $('#epheader').toggleClass('hidden');
+   if(getCookie('epheader')) {
+     deleteCookie('epheader');
+   } else {
+     setCookie('epheader','on');
+   }
   });
   $( "#tabs" ).tabs();
   $('#notif_form').submit(function() {
