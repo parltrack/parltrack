8c8038b7a4b52a9d904ba6642cee18582c6c5cd3
Adam Tauber <asciimoo@gmail.com>
2018-05-07 14:04:38 +0200

    [enh] add webapp helpers and template filters for MEP view

--- webapp.py
+++ webapp.py
@@ -8,8 +8,14 @@ import os
 import config
 import model
 
-from flask import Flask, render_template, request
+from datetime import datetime
 from logging import Formatter, FileHandler
+from sys import version_info
+
+from flask import Flask, render_template, request
+
+if version_info[0] == 3:
+    unicode = str
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -41,7 +47,8 @@ def dumps():
 
 @app.route('/meps')
 def meps():
-    return render_template('meps.html')
+    date = getDate()
+    return render_template('meps.html', date=date)
 
 
 @app.route('/dossiers')
@@ -62,7 +69,112 @@ def not_found_error(error):
     return render_template('errors/404.html'), 404
 
 
-if not app.debug:
+@app.template_filter()
+def asdate(value):
+    if isinstance(value, int):
+        value=datetime.fromtimestamp(value)
+    if type(value) not in [str, unicode]:
+        return value.strftime('%Y/%m/%d')
+    return value.split(' ')[0]
+
+
+@app.template_filter()
+def asdiff(obj): # should have a new and old item
+    de=diff_match_patch.diff_match_patch()
+    diffs=de.diff_main(' '.join (obj.get('old','')),' '.join (obj.get('new','')))
+    de.diff_cleanupSemantic(diffs)
+    return de.diff_prettyHtml(diffs)
+
+
+@app.template_filter()
+def asPE(obj): # should have a new and old item
+    return urllib.unquote(obj).split('+')[2]
+
+
+@app.template_filter()
+def asmep(value):
+    if value in mepcache:
+        mep=mepcache[value]
+    else:
+        db = connect_db()
+        if isinstance(value, int):
+            mep=db.ep_meps2.find_one({'UserID': value}, {'changes': False})
+        else:
+            mep=db.ep_meps2.find_one({'_id': value}, {'changes': False})
+        #if not mep:
+        #    mep=db.ep_meps.find_one({'_id': value}, {'changes': False})
+        if not mep:
+            return value
+        mepcache[value]=mep
+    return u'<a href="/mep/%s">%s</a>' % (mep['Name']['full'],mep['Name']['full'])
+
+
+@app.template_filter()
+def asdossier(value):
+    db = connect_db()
+    doc=db.dossiers2.find_one({'procedure.reference': value})
+    if not doc:
+        return value
+    return (u'<a href="/dossier/%s">%s</a> %s' %
+            (doc['procedure']['reference'],
+             doc['procedure']['reference'],
+             doc['procedure']['title']))
+
+
+@app.template_filter()
+def isodate(value):
+    if type(value)==type(datetime(1,1,1)):
+        return value.isoformat()
+    return datetime.strptime(value,'%Y-%m-%d').isoformat()
+
+
+@app.template_filter()
+def group_icon(value):
+    if not value: return ''
+    if type(value)==type(list()): value=value[0]
+    if value=='NA': value='NI'
+    return "static/images/%s.gif" % value.lower().replace('/','_')
+
+
+@app.template_filter()
+def protect_email(email_address):
+    character_set = '+-.0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz'
+    char_list = list(character_set)
+    shuffle(char_list)
+
+    key = ''.join(char_list)
+
+    cipher_text = ''
+    id = 'e' + str(randrange(1,999999999))
+
+    for a in email_address:
+        cipher_text += key[ character_set.find(a) ]
+
+    script = 'var a="'+key+'";var b=a.split("").sort().join("");var c="'+cipher_text+'";var d="";'
+    script += 'for(var e=0;e<c.length;e++)d+=b.charAt(a.indexOf(c.charAt(e)));'
+    script += 'document.getElementById("'+id+'").innerHTML="<a href=\\"mailto:"+d+"\\">"+d+"</a>"'
+
+
+    script = "eval(\""+ script.replace("\\","\\\\").replace('"','\\"') + "\")"
+    script = '<script type="text/javascript">/*<![CDATA[*/'+script+'/*]]>*/</script>'
+
+    return '<span id="'+ id + '">[javascript protected email address]</span>'+ script
+
+
+def getDate():
+    date=datetime.now()
+    if request.args.get('date'):
+        try:
+            date=datetime.strptime(request.args['date'], "%d/%m/%Y")
+        except ValueError:
+            try:
+                date=datetime.strptime(request.args['date'], "%Y-%m-%d")
+            except ValueError:
+                date=datetime.strptime(request.args['date'], "%Y/%m/%d")
+    return date
+
+
+if not config.DEBUG:
     app.logger.setLevel(logging.INFO)
 
 #----------------------------------------------------------------------------#
