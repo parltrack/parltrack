a355d579b388e3096d25c2ff346c4522cf27dc36
stf <7o5rfu92t@ctrlc.hu>
2019-02-10 22:40:16 +0100

    [enh] added mapping for buildings in utils/mappings.py

---
+++ utils/mappings.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#    This file is part of parltrack
+
+#    parltrack is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    parltrack is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with parltrack  If not, see <http://www.gnu.org/licenses/>.
+
+# (C) 2019 by Stefan Marsiske, <parltrack@ctrlc.hu>
+
+buildings={ u"Altiero Spinelli": u'ASP',
+            u"Willy Brandt": u'WIB',
+            u"Paul-Henri Spaak": u'PHS',
+            u"Atrium": u"ATR",
+            u"Louise Weiss": u'LOW',
+            u"Winston Churchill": u'WIC',
+            u'Salvador de Madariaga': u"SDM",
+            u"Bât. Altiero Spinelli": u'ASP',
+            u"Bât. Willy Brandt": u'WIB',
+            u"Bât. Paul-Henri Spaak": u'PHS',
+            u"Bât. Atrium": u"ATR",
+            u"Bât. Louise Weiss": u'LOW',
+            u"Bât. Winston Churchill": u'WIC',
+            u'B\xe2t. Salvador de Madariaga': u"SDM",
+            }
