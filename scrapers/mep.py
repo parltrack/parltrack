d87a5728f11d43f71f8f8a38a7521edda83c9a59
stf <7o5rfu92t@ctrlc.hu>
2019-02-10 22:40:46 +0100

    [enh] added address and declaration parsing to mep scraper

--- scrapers/mep.py
+++ scrapers/mep.py
@@ -19,6 +19,7 @@
 
 import re
 from utils.utils import fetch, fetch_raw, unws, jdump
+from utils.mappings import buildings
 from datetime import datetime
 from lxml.html.soupparser import fromstring
 
@@ -34,10 +35,39 @@ def deobfus_mail(txt):
     x = txt.replace('[at]','@').replace('[dot]','.')
     return ''.join(x[len('mailto:'):][::-1])
 
+def parse_addr(root):
+    # addresses
+    addrs = {}
+    for li in root.xpath('//div[@class="ep-a_contacts"]/ul/li'):
+        key = unws(''.join(li.xpath('.//h3//text()')))
+        addrs[key]={}
+        if key in ['Brussels', 'Bruxelles', 'Strasbourg']:
+            addrs[key]['phone']=li.xpath('.//li[@class="ep_phone"]/a/@href')[0][4:]
+            addrs[key]['fax']=li.xpath('.//li[@class="ep_fax"]/a/@href')[0][4:]
+        tmp=[unws(x) for x in li.xpath('.//div[@class="ep_information "]//text()') if len(unws(x))]
+        if key=='Strasbourg':
+            addrs[key][u'Address']=dict(zip([u'Organization',u'Building', u'Office', u'Street',u'Zip1', u'Zip2'],tmp))
+            addrs[key][u'Address']['City']=addrs[key]['Address']['Zip2'].split()[1]
+            addrs[key][u'Address']['Zip2']=addrs[key]['Address']['Zip2'].split()[0]
+            addrs[key][u'Address']['building_code']=buildings.get(addrs[key]['Address']['Building'])
+        elif key==u'Brussels' or key==u'Bruxelles':
+            addrs[key][u'Address']=dict(zip([u'Organization',u'Building', u'Office', u'Street',u'Zip'],tmp))
+            addrs[key][u'Address']['City']=addrs[key]['Address']['Zip'].split()[1]
+            addrs[key][u'Address']['Zip']=addrs[key]['Address']['Zip'].split()[0]
+            addrs[key][u'Address']['building_code']=buildings.get(addrs[key]['Address']['Building'])
+        elif key=='Luxembourg':
+            addrs[key][u'Address']=tmp
+        elif key=='Postal address':
+            addrs[key]=tmp
+    return addrs
+
 def scrape(id):
     # we ignore the /meps/en/<id>/<name>/home path, since we can get all info also from other pages
     url = "http://www.europarl.europa.eu/meps/en/%s/name/cv" % id
-    root = fetch(url)
+    xml = fetch_raw(url) # we have to patch up the returned html...
+    xml = xml.replace("</br>","<br/>") # ...it contains some bad tags..
+    root = fromstring(xml) # ...which make the lxml soup parser drop some branches in the DOM
+
     body = root.xpath('//span[@id="mep-card-content"]/following-sibling::div')[0]
     mep = {
         'MepID'     : id,
@@ -54,12 +84,14 @@ def scrape(id):
         # cv
         'cv': {
             'updated': datetime.strptime(root.xpath('//span[starts-with(text(),"Updated: ")]/text()')[0], u"Updated: %d/%m/%Y"),
-        }
+        },
+        'addresses' : parse_addr(root),
     }
     mep['cv'].update({unws(''.join(title.xpath(".//text()"))): [unws(''.join(item.xpath(".//text()")))
                                                                 for item in title.xpath("../../../article//li")]
                       for title in body.xpath('.//h3')
                       if not unws(''.join(title.xpath(".//text()"))).startswith("Original version : ")})
+
     # assistants
     root = fetch("http://www.europarl.europa.eu/meps/en/%s/name/assistants" % id)
     body = root.xpath('//span[@id="mep-card-content"]/following-sibling::div')[0]
@@ -68,13 +100,27 @@ def scrape(id):
                       for title in body.xpath('.//h3')}
     # declarations
     root = fetch("http://www.europarl.europa.eu/meps/en/%s/name/declarations" % id)
-    for addr in root.xpath('//div[@class="ep-a_contacts"]/ul/li'):
-        print(addr) #, unws(''.join(addr.xpath('.//h3//text()'))))
-
     body = root.xpath('//span[@id="mep-card-content"]/following-sibling::div')[0]
-    #print(jdump(mep))
+    for title in body.xpath('.//h3'):
+        key = unws(''.join(title.xpath('.//text()')))
+        if key == 'Declaration of financial interests':
+            key = 'Financial Declarations'
+        elif key == 'Declarations of participation by Members in events organised by third parties':
+            key = 'Declarations of Participation'
+        else:
+            print("unknown type of declaration:", key, "http://www.europarl.europa.eu/meps/en/%s/name/declarations" % id)
+            key = None
+        if key is not None:
+            mep[key] = []
+            for pdf in title.xpath('../../following-sibling::div//li//a'):
+                name = unws(''.join(pdf.xpath('.//text()')))
+                url = pdf.xpath('./@href')[0]
+                mep[key].append({'title': name, 'url': url})
+                #if key == 'Financial Declarations':
+                #    scraper_service.add_job('findecl', payload={'id':id, 'url', url})
 
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/declarations
+    print(jdump(mep))
+
     # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/history/8
     # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/history/7
     # activities
@@ -92,4 +138,5 @@ def scrape(id):
     # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/other-activities/written-declarations
 
 if __name__ == '__main__':
-    scrape(28390)
+    #scrape(28390)
+    scrape(96779)
