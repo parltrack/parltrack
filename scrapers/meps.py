de1adf2503b49d07a2fa459945c21e81f0105908
stf <7o5rfu92t@ctrlc.hu>
2019-02-10 22:41:19 +0100

    [enh] extracted mep scraper from mep crawler in meps.py

--- scrapers/meps.py
+++ scrapers/meps.py
@@ -17,17 +17,16 @@
 
 # (C) 2019 by Stefan Marsiske, <parltrack@ctrlc.hu>
 
-from utils.utils import unws, jdump
-from lxml.html.soupparser import fromstring
-from datetime import datetime
-#from mappings import COMMITTEE_MAP, buildings, group_map, COUNTRIES, SEIRTNUOC
-#from urllib.parse import urljoin
-#import unicodedata, traceback, sys, json
-#from utils.multiplexer import Multiplexer, logger
-#from model import Mep
-#import findecl
+from utils.utils import fetch
 
-def crawler(all=False):
+CONFIG = {
+    'threads': 8,
+    'timeout': 60,
+    'retries': 5,
+    'error_handler': None,
+}
+
+def scrape(all=False, **kwargs):
     if all:
         sources = ['http://www.europarl.europa.eu/meps/en/directory/xml?letter=&leg=']
     else:
@@ -38,56 +37,11 @@ def crawler(all=False):
         for unlisted in [ 1018, 26833, 1040, 1002, 2046, 23286, 28384, 1866, 28386,
                           1275, 2187, 34004, 28309, 1490, 28169, 28289, 28841, 1566,
                           2174, 4281, 28147, 28302, ]:
-            yield unlisted
+            scraper_service.add_job('mep', payload={'id':unlisted})
     for src in sources:
         root = fetch(src)
         for id in root.xpath("//mep/id/text()"):
-            print int(id)
-
-def crawler_cb(all, txt):
-
-def deobfus_mail(txt):
-    x = txt.replace('[at]','@').replace('[dot]','.')
-    return ''.join(x[len('mailto:'):][::-1])
-
-def scraper(id, txt):
-    root = fromstring(txt[txt.find('?>')+2:])
-    mep = {
-        'name'      : unws(' '.join(root.xpath('//*[@id="name-mep"]//text()'))),
-        'twitter'   : root.xpath('//div[@class="ep_share"]//a[@title="Twitter"]/@href'),
-        'homepage'  : root.xpath('//div[@class="ep_share"]//a[@title="Website"]/@href'),
-        'facebook'  : root.xpath('//div[@class="ep_share"]//a[@title="Facebook"]/@href'),
-        'email'     : [deobfus_mail(x) for x in root.xpath('//div[@class="ep_share"]//a[@title="E-mail"]/@href')],
-        'instagram' : root.xpath('//div[@class="ep_share"]//a[@title="Instagram"]/@href'),
-        'Birth'     : {
-            'date'  : datetime.strptime(root.xpath('//time[@id="birthDate"]/text()')[0], u"%d-%m-%Y"),
-            'place' : root.xpath('//span[@id="birthPlace"]/text()')[0]
-        },
-    }
-
-    print(jdump(mep))
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/home
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/cv
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/declarations
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/assistants
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/history/8
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/history/7
-    # activities
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/main-activities/plenary-speeches
-    # or /meps/en/96674/loadmore-activities/plenary-speeches/8/?from=10&count=100
-    # or /meps/en/96674/loadmore-activities/written-explanations/8/?from=10&count=100
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/main-activities/reports
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/main-activities/reports-shadow
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/main-activities/opinions-shadow
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/main-activities/motions-instit
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/main-activities/oral-questions
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/other-activities/written-explanations
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/other-activities/written-questions-other
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/other-activities/motions-indiv
-    # /meps/en/96674/ANNA+MARIA_CORAZZA+BILDT/other-activities/written-declarations
-    pass
+            scraper_service.add_job('mep', payload={'id':id})
 
 if __name__ == '__main__':
-    #crawler()
-    from utils.utils import fetch_raw
-    scraper(28390, fetch_raw("http://www.europarl.europa.eu/meps/en/%s/name/%s" % (id, 'home')))
+    scrape(False)
