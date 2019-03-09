396a76bc03aa39c6cbd6b7e932f3be0bcb8eb307
stf <7o5rfu92t@ctrlc.hu>
2019-03-10 00:55:28 +0100

    [enh] refactored indexing, implemented primary key generation, put and commit operations in db

--- db.py
+++ db.py
@@ -7,19 +7,13 @@ import os, json, sys, atexit, msgpack, struct
 from datetime import datetime
 from threading import Thread
 from utils.log import log
+from tempfile import mkstemp
+from utils.utils import dateJSONhandler
 
-TABLES = ['ep_amendments', 'ep_comagendas', 'ep_com_votes', 'ep_dossiers', 'ep_meps', 'ep_votes',]
 PIDFILE='db.pid'
 
 DBS = {}
 IDXs = {}
-
-TABLE_KEYS={'ep_amendments': "_id",
-            'ep_comagendas': "_id",
-            'ep_com_votes': "_id",
-            'ep_dossiers': "_id",
-            'ep_meps': "UserID",
-            'ep_votes': "_id"}
 
 def cleanup_singleton():
     log(3,"cleaning up {}".format(PIDFILE))
@@ -52,31 +46,41 @@ def singleton():
         fd.write(str(os.getpid()))
     atexit.register(cleanup_singleton)
 
+def reindex(table):
+    for idx in TABLES[table]['indexes']:
+        IDXs[idx['name']]=idx['fn']()
+
+def reindex_all():
+    for table in TABLES.keys():
+        reindex(table)
+
+def genkey(table):
+    key = random.randrange(2**32)
+    while key in table.keys():
+        key = random.randrange(2**32)
+    return key
+
 def init(data_dir):
     log(3,"initializing")
     singleton()
+
+    global DBDIR
+    DBDIR = data_dir
+
     # load json dumps int global DBS dict
-    for table in TABLES:
+    for table in TABLES.keys():
         with open("{}/{}.json".format(data_dir, table), 'rt') as fd:
             log(3,"loading table {}".format(table))
-            DBS[table]={item[TABLE_KEYS[table]]: item for item in json.load(fd)}
+            DBS[table]={item[TABLES[table]['key']]: item for item in json.load(fd)}
 
     # initialize indexes
     log(3,"indexing tables")
-    IDXs['meps_by_activity']=idx_meps_by_activity()
-    IDXs['meps_by_country']=idx_meps_by_country()
-    IDXs['meps_by_group']=idx_meps_by_group()
-    IDXs['ams_by_group']=idx_ams_by_dossier()
-    IDXs['ams_by_group']=idx_ams_by_mep()
-    IDXs['com_votes_by_dossier']=idx_com_votes_by_dossier()
-    IDXs['com_votes_by_committee']=idx_com_votes_by_committee()
-    IDXs['votes_by_dossier']=idx_votes_by_dossier()
-    IDXs['idx_dossiers_by_reference'] = idx_dossiers_by_reference()
+    reindex_all()
     log(3,"init done")
 
 def read_req(sock):
     size = sock.recv(4)
-    log(4, "req size is {!r}".format(size))
+    log(5, "req size is {!r}".format(size))
     size = struct.unpack("I", size)[0]
     if size > 1024 * 1024 * 50: # arbitrary upper limit for request 50MB
         log(1, "request is too big: {}MB".format(size / (1024*1024)))
@@ -92,8 +96,6 @@ def read_req(sock):
     return req
 
 def mainloop():
-    # TODO handle incoming requests
-    #while(True): pass
     from IPython import embed
     Thread(target=embed).start()
 
@@ -129,14 +131,19 @@ def mainloop():
             log(3, 'connection from {}'.format(client_address))
             query = read_req(connection)
             if query.get('cmd', '') == 'get':
-                tmp = get(**query.get('params', [])) or None
-                log(3,"responding with {} records".format(len(tmp) if tmp else tmp))
-                fd = connection.makefile(mode = 'wb', buffering = 65535)
-                msgpack.dump(tmp, fd, use_bin_type = True)
-                log(3,'sent data back to the client')
-                fd.close()
+                res = get(**query.get('params', [])) or None
+            elif query.get('cmd', '') == 'put':
+                res = put(**query.get('params', [])) or None
+            elif query.get('cmd', '') == 'commit':
+                res = commit(**query.get('params', [])) or None
             else:
                 log(2,'invalid or missing cmd')
+                continue
+            log(3,"responding with {} records".format(len(res) if res else res))
+            fd = connection.makefile(mode = 'wb', buffering = 65535)
+            msgpack.dump(res, fd, use_bin_type = True)
+            log(3,'sent data back to the client')
+            fd.close()
         except:
             log(1, "connection error")
             traceback.print_exc()
@@ -159,6 +166,32 @@ def get(source, key):
             return DBS[source].get(key)
     log(1, 'source not found in db nor in index')
     return None
+
+def put(table, value):
+    # TODO error handling
+    log(3,'storing into src: "{}" key: "{}"'.format(table,key))
+    if not table in DBS:
+        log(1, 'table not found in db')
+        return False
+    key = value[TABLES[table].get('key', genkey(table))]
+    DBS[table][key]=value
+    reindex(table)
+    return True
+
+def commit(table):
+    if not table in DBS:
+        log(1, 'table not found in db')
+        return False
+    def jdump(obj):
+        return json.dumps(obj, default=dateJSONhandler, ensure_ascii=False).encode('utf8')
+    (fd, name) = mkstemp(dir=DBDIR)
+    items = DBS[table].values()
+    fd.write(b"[{}".format(jdump(items[0])))
+    for rec in DBS[table].values()[1:]:
+        fd.write(b'\n,{}'.format(jdump(rec)))
+    fd.write(b'\n]')
+    fd.close()
+    os.rename(name, "{}/{}.json".format(DBDIR, table))
 
 ######  indexes ######
 
@@ -250,6 +283,29 @@ def idx_dossiers_by_reference():
     return res
 # todo indexes for dossiers by committees, rapporteur, subject, stage_reached
 # todo indexes for meps by name(alias)
+
+TABLES = {'ep_amendments': {'indexes': [{"fn": idx_ams_by_dossier, "name": "ams_by_dossier"},
+                                        {"fn": idx_ams_by_mep, "name": "ams_by_mep"}],
+                            'key': '_id'},
+
+          'ep_comagendas': {"indexes": [],
+                            'key': '_id'},
+
+          'ep_com_votes': {'indexes': [{"fn": idx_com_votes_by_dossier, "name": "com_votes_by_dossier"},
+                                       {"fn": idx_com_votes_by_committee, "name": "com_votes_by_committee"}],
+                           'key': '_id'},
+
+          'ep_dossiers': {'indexes': [{"fn": idx_dossiers_by_reference, "name": "idx_dossiers_by_reference"}],
+                          'key': '_id'},
+
+          'ep_meps': {'indexes': [{"fn": idx_meps_by_activity, "name": "meps_by_activity"},
+                                  {"fn": idx_meps_by_country, "name": "meps_by_country"},
+                                  {"fn": idx_meps_by_group, "name": "meps_by_group"}],
+                      'key': 'UserID'},
+
+          'ep_votes': {'indexes': [{"fn": idx_votes_by_dossier, "name": "votes_by_dossier"}],
+                       'key': '_id'},
+}
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'dev':
