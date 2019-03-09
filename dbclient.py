2960d3a4c9e008e59d140d1ce38ab8181d1c2186
stf <7o5rfu92t@ctrlc.hu>
2019-03-10 00:55:58 +0100

    [enh] refactored sending requests, implemented api for put and commit ops in dbclient

--- dbclient.py
+++ dbclient.py
@@ -4,31 +4,42 @@ import socket, sys, msgpack, struct
 from utils.log import log
 
 class DB:
+    def commit(self, table):
+        cmd = {"cmd": "get", "params": {"table": table}}
+        return self.send_req(sock, cmd)
+
+    def put(self, table, value):
+        cmd = {"cmd": "get", "params": {"table": table, "value": value}}
+        return self.send_req(sock, cmd)
+
     def get(self, source, key):
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        cmd = {"cmd": "get", "params": {"key": key, "source": source}}
+        return self.send_req(sock, cmd)
+
+    def send_req(self, cmd):
         server_address = '/tmp/pt-db.sock'
-        log(3,'connecting to db on {}'.format(server_address))
-        sock.connect(server_address)
+        req = msgpack.dumps(cmd, use_bin_type = True)
+
         try:
+            sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            log(3,'connecting to db on {}'.format(server_address))
+            sock.connect(server_address)
+
+            log(3,'sending {!r}'.format(cmd))
+            sock.sendall(struct.pack("I", len(req)))
+            sock.sendall(req)
+            log(3,'sent {} bytes'.format(len(req)+4))
+
             # Send request
-            cmd = {"cmd": "get", "params": {"key": key, "source": source}}
-            self.send_req(sock, cmd)
             # make the sock a file object
             fd = sock.makefile(mode = 'rb', buffering = 65535)
             # unmarshall response
             res = msgpack.load(fd, raw = False)
+            fd.close()
         finally:
             log(3,'closing socket')
             sock.close()
-            fd.close()
             return res
-
-    def send_req(self, sock, cmd):
-        log(3,'sending {!r}'.format(cmd))
-        req = msgpack.dumps(cmd, use_bin_type = True)
-        sock.sendall(struct.pack("I", len(req)))
-        sock.sendall(req)
-        log(3,'sent {} bytes'.format(len(req)+4))
 
     def meps_by_activity(self,key=True):
         return db.get('meps_by_activity', "active" if key else "inactive")
