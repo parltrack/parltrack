26f74f5db3fed3f657ddd188e0273c4e3a54d53e
ParlTrack <parltrack@ctrlc.hu>
2019-03-28 14:35:25 +0000

    [mod] moved dbclient into db. added dossiers idxes, keys are lambdas for deeper keys, and can be none

--- db.py
+++ db.py
@@ -3,22 +3,86 @@
 import socket
 import sys
 import traceback
-import os, json, sys, atexit, msgpack, struct, stat
+import os, json, sys, atexit, msgpack, struct, stat, unicodedata
 from random import randrange
-from datetime import datetime
+from datetime import datetime, date, timedelta
 from threading import Thread
 from utils.log import log
 from tempfile import mkstemp
 from utils.utils import dateJSONhandler
+
 
 PIDFILE='db.pid'
 
 DBS = {}
 IDXs = {}
 
+
+class Client:
+    def commit(self, table):
+        cmd = {"cmd": "commit", "params": {"table": table}}
+        return self.send_req(cmd)
+
+    def put(self, table, value):
+        cmd = {"cmd": "put", "params": {"table": table, "value": value}}
+        return self.send_req(cmd)
+
+    def get(self, source, key):
+        cmd = {"cmd": "get", "params": {"key": key, "source": source}}
+        return self.send_req(cmd)
+
+    def send_req(self, cmd):
+        server_address = '/tmp/pt-db.sock'
+        req = msgpack.dumps(cmd, default=dateJSONhandler, use_bin_type = True)
+
+        try:
+            sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            log(4,'connecting to db on {}'.format(server_address))
+            sock.connect(server_address)
+
+            log(4,'sending {} bytes: {}...'.format(len(req), repr(cmd)[:120]))
+            sock.sendall(struct.pack("I", len(req)))
+            sock.sendall(req)
+            log(4,'sent {} bytes'.format(len(req)+4))
+
+            # Send request
+            # make the sock a file object
+            fd = sock.makefile(mode = 'rb', buffering = 65535)
+            # unmarshall response
+            res = msgpack.load(fd, raw = False)
+            fd.close()
+        except:
+            log(1, "error during processing request {}".format(cmd))
+            raise
+        else:
+            log(4,'closing socket')
+            sock.close()
+            return res
+
+    def meps_by_activity(self,key=True):
+        return self.get('meps_by_activity', "active" if key else "inactive")
+
+    def mep(self,id):
+        return self.get('ep_meps', id)
+
+    def mepid_by_name(self, name, date=None, group=None):
+        # normalize name
+        name = ''.join(unicodedata.normalize('NFKD', name.replace(u'ß','ss')).encode('ascii','ignore').decode('utf8').split()).lower()
+        return self.get('mepid_by_name', (name,group,date))
+
+    def activities(self,id):
+        return self.get('ep_mep_activities', id)
+
+    def dossiers_by_activity(self,key=True):
+        return self.get('active_dossiers', "active" if key else "inactive")
+
+    def dossier_refs(self):
+        return self.get('dossier_refs', None)
+
 def cleanup_singleton():
     log(3,"cleaning up {}".format(PIDFILE))
     os.unlink(PIDFILE)
+
 
 def singleton():
     # ensure we are a singleton
@@ -72,7 +136,7 @@ def init(data_dir):
     for table in TABLES.keys():
         with open("{}/{}.json".format(data_dir, table), 'rt') as fd:
             log(3,"loading table {}".format(table))
-            DBS[table]={item[TABLES[table]['key']]: item for item in json.load(fd)}
+            DBS[table]={TABLES[table]['key'](item): item for item in json.load(fd)}
 
     # initialize indexes
     log(3,"indexing tables")
@@ -162,6 +226,11 @@ def get(source, key):
             return [IDXs[source].get(x) for x in key]
         if source in DBS:
             return [DBS[source].get(x) for x in key]
+    elif key == None:
+        if source in IDXs:
+            return IDXs[source]
+        if source in DBS:
+            return DBS[source]
     else:
         if source in IDXs:
             return IDXs[source].get(key)
@@ -175,7 +244,7 @@ def put(table, value):
     if not table in DBS:
         log(1, 'table not found in db')
         return False
-    key = value[TABLES[table].get('key', genkey(table))]
+    key = TABLES[table]['key'](value) or genkey(table)
     log(3,'storing into src: "{}" key: "{}"'.format(table,key))
     DBS[table][key]=value
     reindex(table)
@@ -231,6 +300,39 @@ def idx_meps_by_group():
             res[group].append({k:v for k,v in mep.items() if k not in ['changes', 'activities']})
     return res
 
+def idx_mepid_by_name():
+    return # todo remove this! important :)
+    res={}
+    for mep in DBS['ep_meps'].values():
+        mepid = mep['UserID']
+        name = ''.join(unicodedata.normalize('NFKD', mep['Name']['full'].replace(u'ß','ss')).encode('ascii','ignore').decode('utf8').split()).lower()
+        gd = {}
+        for g in mep.get('Groups', []):
+            if not g:
+                print(mepid, name, "has none in groups")
+                continue
+            gid = g['groupid']
+            gs = date.fromisoformat(g['start'][:-9])
+            ge = date.today() if g['end'] == '31-12-9999T00:00:00' else date.fromisoformat(g['end'][:-9])
+            for n in range(int ((ge - gs).days)+1):
+                gd[gs + timedelta(n)] = gid
+        # for sanity check
+        inoffice = set()
+        for c in mep.get('Constituencies',[]):
+            if not c:
+                print(mepid, name, "has none in constituencies")
+                continue
+            cs = date.fromisoformat(c['start'][:-9])
+            ce = date.today() if c['end'] == '31-12-9999T00:00:00' else date.fromisoformat(c['end'][:-9])
+            for n in range(int ((ce - cs).days)+1):
+                inoffice.add(cs + timedelta(n))
+        gdset=set(gd.keys())
+        notingdset = inoffice - gdset
+        notinoffice = gdset - inoffice
+        if notinoffice: print("notinoffice", mepid, name, sorted([x.isoformat() for x in notinoffice]))
+        if notingdset: print("notingdset", mepid, name, sorted([x.isoformat() for x in notingdset]))
+        # end sanitycheck
+
 def idx_ams_by_mep():
     res = {}
     for am in DBS['ep_amendments'].values():
@@ -281,12 +383,14 @@ def idx_votes_by_dossier():
         res[dossier].append(vote)
     return res
 
-def idx_dossiers_by_reference():
-    res = {}
+def idx_active_dossiers():
+    # procedure.stage_reached not in [ "Procedure completed", "Procedure rejected", "Procedure lapsed or withdrawn"]
+    res = {'active': [], 'inactive': []}
     for dossier in DBS['ep_dossiers'].values():
-        reference = dossier['procedure']['reference']
-        if not reference in res: res[reference] = []
-        res[reference].append(dossier)
+        if dossier['procedure']['stage_reached'] in [ "Procedure completed", "Procedure rejected", "Procedure lapsed or withdrawn"]:
+            res['inactive'].append(dossier)
+        else:
+            res['active'].append(dossier)
     return res
 
 def idx_activities_by_mep():
@@ -297,34 +401,46 @@ def idx_activities_by_mep():
         res[mep_id].append(act)
     return res
 
+def idx_dossier_refs():
+    res = []
+    for doc in DBS['ep_dossiers'].values():
+        res.append(doc['procedure']['reference'])
+    return res
+
 # todo indexes for dossiers by committees, rapporteur, subject, stage_reached
 # todo indexes for meps by name(alias)
 
 TABLES = {'ep_amendments': {'indexes': [{"fn": idx_ams_by_dossier, "name": "ams_by_dossier"},
                                         {"fn": idx_ams_by_mep, "name": "ams_by_mep"}],
-                            'key': '_id'},
+                            'key': lambda x: x.get('_id')},
 
           'ep_comagendas': {"indexes": [],
-                            'key': '_id'},
+                            'key': lambda x: x.get('_id')},
 
           'ep_com_votes': {'indexes': [{"fn": idx_com_votes_by_dossier, "name": "com_votes_by_dossier"},
                                        {"fn": idx_com_votes_by_committee, "name": "com_votes_by_committee"}],
-                           'key': '_id'},
+                           'key': lambda x: x.get('_id')},
 
-          'ep_dossiers': {'indexes': [{"fn": idx_dossiers_by_reference, "name": "idx_dossiers_by_reference"}],
-                          'key': '_id'},
+          'ep_dossiers': {'indexes': [{"fn": idx_active_dossiers, "name": "active_dossiers"},
+                                      {"fn": idx_dossier_refs, "name": "dossier_refs"},
+
+          ],
+                          'key': lambda x: x['procedure']['reference']}, # todo change to procedure/reference
 
           'ep_meps': {'indexes': [{"fn": idx_meps_by_activity, "name": "meps_by_activity"},
                                   {"fn": idx_meps_by_country, "name": "meps_by_country"},
-                                  {"fn": idx_meps_by_group, "name": "meps_by_group"}],
-                      'key': 'UserID'},
+                                  {"fn": idx_meps_by_group, "name": "meps_by_group"},
+                                  {"fn": idx_mepid_by_name, "name": "mepid_by_name"}],
+                      'key': lambda x: x['UserID']},
 
           'ep_mep_activities': {'indexes': [{"fn": idx_activities_by_mep, "name": "activities_by_mep"},],
-                      'key': 'mep_id'},
+                                'key': lambda x: x['mep_id']},
 
           'ep_votes': {'indexes': [{"fn": idx_votes_by_dossier, "name": "votes_by_dossier"}],
-                       'key': '_id'},
+                       'key': lambda x: x.get('_id')},
 }
+
+db = Client()
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'dev':
