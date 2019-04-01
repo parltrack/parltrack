63412672a2a82536a22f55ce9f78b94dd31183d8
ParlTrack <parltrack@ctrlc.hu>
2019-04-01 12:55:38 +0000

    [enh] add dossier and mep view

--- webapp.py
+++ webapp.py
@@ -6,18 +6,24 @@ import logging
 import os
 
 import config
-from model import session, Dossier, MEP, PartyMEP
 
 from pprint import pprint
-from datetime import datetime
+from datetime import datetime, date
 from flask import Flask, render_template, request
 from logging import Formatter, FileHandler
+from random import shuffle, randrange
 from sys import version_info
+from urllib.parse import unquote
+import diff_match_patch
 
-from flask import Flask, render_template, request
-from sqlalchemy import and_
-
+from flask import Flask, render_template, request, redirect
 from utils.utils import asDate
+from utils.mappings import (
+    SEIRTNUOC as COUNTRIES,
+    COMMITTEE_MAP,
+)
+
+from db import Client
 
 if version_info[0] == 3:
     unicode = str
@@ -28,6 +34,8 @@ if version_info[0] == 3:
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+db = Client()
 
 
 def render(template, **kwargs):
@@ -41,10 +49,10 @@ def render(template, **kwargs):
 
 @app.route('/')
 def home():
-    meps = session.query(MEP).count()
-    dossiers = session.query(Dossier).count()
+    meps = 0
+    dossiers = 0
     date = getDate()
-    active_meps = session.query(MEP).filter(MEP.parties.any(and_(PartyMEP.begin < date, PartyMEP.end > date))).count()
+    active_meps = 0
     return render_template(
         'index.html',
         mep_count=meps,
@@ -65,19 +73,56 @@ def dumps():
 
 @app.route('/meps')
 def meps():
+    # TODO date handling
     date = getDate()
-    meps = session.query(MEP).filter(MEP.parties.any(and_(PartyMEP.begin < date, PartyMEP.end > date))).order_by(MEP.full_name).all()
+    meps = db.meps_by_activity(True)
     return render_template('meps.html', date=date, meps=meps)
 
 
-@app.route('/mep')
-def mep():
-    return
+@app.route('/mep/<int:mep_id>')
+def mep(mep_id):
+    mep = db.mep(mep_id)
+    if not mep:
+        return not_found_error(None)
+    mep['amendments'] = db.get("ams_by_mep", mep_id) or []
+    return render_template(
+        'mep.html',
+        mep=mep,
+        today=date.today().strftime("%Y-%m-%d"),
+        countries=COUNTRIES,
+        d=mep_id,
+        group_cutoff=datetime(2004,7,20).strftime("%Y-%m-%d"),
+        # TODO
+        committees={},
+        committee_map=COMMITTEE_MAP,
+    )
+
+
+@app.route('/mep/<string:mep_name>')
+def mep_name(mep_name):
+    meps = db.mepid_by_name(mep_name)
+    if meps:
+        return redirect('/mep/{0}'.format(meps[0]))
 
 
 @app.route('/dossiers')
-def dossiers():
+def dossiers(d_id):
     return render('dossiers.html')
+
+
+@app.route('/dossier/<path:d_id>')
+def view_dossier(d_id):
+    d = db.dossier(d_id)
+    if not d:
+        return not_found_error(None)
+    d['amendments'] = db.get("ams_by_dossier", d_id) or []
+    return render(
+        'dossier.html',
+        dossier=d,
+        d=d_id,
+        url=request.base_url,
+        now_date=date.today().strftime("%Y-%m-%d"),
+    )
 
 
 # Error handlers.
@@ -109,6 +154,23 @@ def getDate():
 
 
 @app.template_filter()
+def printdict(d):
+    if type(d)==type(list()):
+        return u'<ul>%s</ul>' % '\n'.join(["<li>%s</li>" % printdict(v) for v in d])
+    if type(d)==type(datetime(2000,1,1)):
+        return "%s" % d.isoformat()[:10]
+    elif not type(d)==type(dict()):
+        return "%s" % unicode(d)
+    res=['']
+    for k,v in [(k,v) for k,v in d.items() if k not in ['mepref','comref']]:
+        if type(v) == type(dict()) or (type(v)==type(list()) and len(v)>1):
+            res.append(u"<dl><dt class='more'>%s</dt><dd class='hidden'>%s</dd></dl>" % (k,printdict(v)))
+        else:
+            res.append(u"<dl><dt>%s</dt><dd>%s</dd></dl>" % (k,printdict(v)))
+    return '%s' % u'\n'.join(res)
+
+
+@app.template_filter()
 def asdate(value):
     if type(value)==type(int()):
         value=datetime.fromtimestamp(value)
@@ -136,31 +198,26 @@ def asdiff(obj): # should have a new and old item
 
 @app.template_filter()
 def asPE(obj): # should have a new and old item
-    return urllib.unquote(obj).split('+')[2]
+    return unquote(obj).split('+')[2]
 
 
 @app.template_filter()
 def asmep(value):
-    if value in mepcache:
-        mep=mepcache[value]
-    else:
-        db = connect_db()
-        if isinstance(value, int):
-            mep=db.ep_meps2.find_one({'UserID': value}, {'changes': False})
-        else:
-            mep=db.ep_meps2.find_one({'_id': value}, {'changes': False})
-        #if not mep:
-        #    mep=db.ep_meps.find_one({'_id': value}, {'changes': False})
-        if not mep:
-            return value
-        mepcache[value]=mep
+    #if not hasattr(request, 'meps'):
+    #    request.meps = {}
+    #if value in request.meps:
+    #    mep = request.meps[value]
+    #else:
+    #    mep = db.get('ep_meps', value)
+    #    request.meps[value] = mep
+    mep = db.get('ep_meps', value)
     return u'<a href="/mep/%s">%s</a>' % (mep['Name']['full'],mep['Name']['full'])
 
 
+# TODO
 @app.template_filter()
 def asdossier(value):
-    db = connect_db()
-    doc=db.dossiers2.find_one({'procedure.reference': value})
+    doc=db.get('ep_dossiers', value)
     if not doc:
         return value
     return (u'<a href="/dossier/%s">%s</a> %s' %
@@ -173,7 +230,7 @@ def asdossier(value):
 def isodate(value):
     if type(value)==type(datetime(1,1,1)):
         return value.isoformat()
-    return datetime.strptime(value,'%Y-%m-%d').isoformat()
+    return datetime.strptime(value[:10],'%Y-%m-%d').isoformat()
 
 
 @app.template_filter()
@@ -208,10 +265,16 @@ def protect_email(email_address):
 
     return '<span id="'+ id + '">[javascript protected email address]</span>'+ script
 
+
+@app.template_filter()
+def reftopath(ref):
+    return "%s/%s" % (ref[-4:-1], ref[:9])
+
+
 def getDate():
     date=datetime.now()
     if request.args.get('date'):
-        date = asDate(request.args['date']
+        date = asDate(request.args['date'])
     return date
 
 if not config.DEBUG:
