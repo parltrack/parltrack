651ad27106ab76e2e2533435a702286f62efd160
ParlTrack <parltrack@ctrlc.hu>
2019-04-14 22:55:11 +0000

    [mod] removed old db related config settings

--- config.py
+++ config.py
@@ -14,22 +14,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 DB_DEBUG = False
 
-#SECRET_KEY = os.environ.get('SECRET', 'my precious')
-DB_USER = os.environ.get('DB_USER', '')
-DB_PASS = os.environ.get('DB_PASS', '')
-DB_HOST = os.environ.get('DB_HOST', '127.0.0.1')
-DB_DATABASE = os.environ.get('DB_DATABASE', 'parltrack')
-DB_PORT = os.environ.get('DB_PORT', '5432')
-
 # Webapp config
 
 WEBSERVER_PORT = 6776
-
-# Connect to the database
-SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}".format(
-    DB_USER,
-    DB_PASS,
-    DB_HOST,
-    DB_PORT,
-    DB_DATABASE,
-)
