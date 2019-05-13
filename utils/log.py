b716eed1586e36df0ae036920eb8b7e1b4236063
ParlTrack <parltrack@ctrlc.hu>
2019-05-13 22:16:21 +0000

    [enh] log: added setlogfile and writing to it

--- utils/log.py
+++ utils/log.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import inspect
 
 loglevel=4 # info
+logfile = None
 
 LEVELS = ('quiet', 'error', 'warning', 'info', 'debug')
 
@@ -25,11 +26,23 @@ def log(level, msg):
             break
     else:
         print("{ts} log error unknown module: {stack}".format(ts=datetime.isoformat(datetime.now()), stack=inspect.stack()))
+        if logfile:
+            logfile.write("{ts} log error unknown module: {stack}\n".format(ts=datetime.isoformat(datetime.now()), stack=inspect.stack()))
         scraper = '???'
 
     if level <= loglevel:
         print("{ts} {scraper} {level} {msg}".format(ts=datetime.isoformat(datetime.now()), level=LEVELS[level], scraper=scraper, msg=msg))
+        if logfile:
+            logfile.write("{ts} {scraper} {level} {msg}\n".format(ts=datetime.isoformat(datetime.now()), level=LEVELS[level], scraper=scraper, msg=msg))
 
 def set_level(l):
     global loglevel
     loglevel=l
+
+def set_logfile(l):
+    global logfile
+    if logfile: 
+        logfile.close()
+        logfile=None
+    if l:
+        logfile=open(l,'w')
