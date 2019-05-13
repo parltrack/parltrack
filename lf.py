ed25270810f0ee0de30fa412c7323ccae4284950
ParlTrack <parltrack@ctrlc.hu>
2019-05-13 22:12:04 +0000

    [enh] lf now reads stdin instead of explicit file from cl

--- lf.py
+++ lf.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from utils.log import LEVELS
 
 def usage():
-    print("%s <debug|info|warn(ing)|err(or)> logfile")
+    print("%s <debug|info|warn(ing)|err(or)> <logfile")
     sys.exit(0)
 
 def parse(line):
@@ -16,7 +16,7 @@ def parse(line):
         date = datetime.fromisoformat(tokens[0])
     except:
         return line
-    if tokens[1] not in ['db','dossier','dossiers','mep','meps','mgr']:
+    if tokens[1] not in ['db','dossier','dossiers','mep','meps','mgr','pvote','pvotes']:
         return line
     module = tokens[1]
     if tokens[2] not in LEVELS:
@@ -37,7 +37,9 @@ def dump(date, module, level, levelz, msg):
         "mep": "mep",
         "meps": "\033[38;2;127;127;127mmeps\033[0m",
         "dossier": "dossier",
-        "dossiers": "\033[38;2;127;127;127mdossiers\033[0m",
+        "dossiers": "\033[38;2;127;127;127mpvotes\033[0m",
+        "pvote": "pvote",
+        "pvotes": "\033[38;2;127;127;127mpvotes\033[0m",
     }
     if dlevel<level:
         return
@@ -45,7 +47,7 @@ def dump(date, module, level, levelz, msg):
 
 dlevel = 0
 fname = None
-for arg in sys.argv:
+for arg in sys.argv[1:]:
     if arg == 'help':
         usage()
     elif arg == 'debug':
@@ -56,15 +58,9 @@ for arg in sys.argv:
         dlevel=2
     elif arg in ['err', 'error']:
         dlevel=1
-    elif exists(arg):
-        fname = arg
     else:
         print('unknown parameter "%s"' % repr(arg))
         usage()
-
-if not fname:
-    print('input logfile must be specified')
-    usage()
 
 stats = {l:0 for l in LEVELS}
 changes = {}
@@ -72,66 +68,66 @@ nobody = {}
 plev = 0
 insummary = False
 exceptions=0
-with open(fname,'r') as f:
-    buffer = []
-    for line in f:
-        tmp = parse(line)
-        if tmp==line:
-            if line.strip()=='Traceback (most recent call last):':
-                plev=0
-                exceptions+=1
-                buffer.append(line[:-1])
-            # some other string
-            if dlevel>=plev: # only do stuff if we would print this out anyway
-                if not insummary: # strip out summaries
-                    if line.strip() == '"summary": [':
-                        buffer.append(line[:-1]+'...\033[48;2;127;127;127mstripped\033[0m...')
-                        insummary=True
-                        continue
-                else:
-                    if line.strip() in [']', '],']:
-                        insummary=False
+
+buffer = []
+for line in sys.stdin:
+    tmp = parse(line)
+    if tmp==line:
+        if line.strip()=='Traceback (most recent call last):':
+            plev=0
+            exceptions+=1
+            buffer.append(line[:-1])
+        # some other string
+        if dlevel>=plev: # only do stuff if we would print this out anyway
+            if not insummary: # strip out summaries
+                if line.strip() == '"summary": [':
+                    buffer.append(line[:-1]+'...\033[48;2;127;127;127mstripped\033[0m...')
+                    insummary=True
                     continue
-                buffer.append(line[:-1])
-        else:
-            if buffer:
-                # handle buffer
-                if dlevel>=plev:
-                    t = '\n'.join(buffer).strip()
-                    if t: print("%s\n" % t)
-                buffer=[]
-            (date, module, level, levelz, msg) = tmp
-            stats[levelz]+=1
-            if msg.startswith('no body mapping found for'):
-                if msg[25:] not in nobody: nobody[msg[25:]]=0
-                nobody[msg[25:]]+=1
-            if msg.startswith("preserving deleted path ['other'] for obj id:"): # todo remove
+            else:
+                if line.strip() in [']', '],']:
+                    insummary=False
                 continue
-            if module in ['dossier','mep'] and level==3 and msg.startswith("adding "):
-                if module not in changes: changes[module]={'added':0, 'updated':0}
-                changes[module]['added']+=1
-            if module in ['dossier','mep'] and level==3 and msg.startswith("updating "):
-                if module not in changes: changes[module]={'added':0, 'updated':0}
-                changes[module]['updated']+=1
-            dump(date, module, level, levelz, msg)
-            plev = level
+            buffer.append(line[:-1])
+    else:
+        if buffer:
+            # handle buffer
+            if dlevel>=plev:
+                t = '\n'.join(buffer).strip()
+                if t: print("%s\n" % t)
+            buffer=[]
+        (date, module, level, levelz, msg) = tmp
+        stats[levelz]+=1
+        if msg.startswith('no body mapping found for'):
+            if msg[25:] not in nobody: nobody[msg[25:]]=0
+            nobody[msg[25:]]+=1
+        #if msg.startswith("preserving deleted path ['other'] for obj id:"): # todo remove
+        #    continue
+        if module in ['dossier','mep','pvote'] and level==3 and msg.startswith("adding "):
+            if module not in changes: changes[module]={'added':0, 'updated':0}
+            changes[module]['added']+=1
+        if module in ['dossier','mep','pvote'] and level==3 and msg.startswith("updating "):
+            if module not in changes: changes[module]={'added':0, 'updated':0}
+            changes[module]['updated']+=1
+        dump(date, module, level, levelz, msg)
+        plev = level
 
-    if buffer:
-        # handle buffer
-        if dlevel>=plev:
-            t = '\n'.join(buffer).strip()
-            if t: print("%s\n" % t)
+if buffer:
+    # handle buffer
+    if dlevel>=plev:
+        t = '\n'.join(buffer).strip()
+        if t: print("%s\n" % t)
 
-    # print stats
-    print("stats")
-    for mod, s in changes.items():
-        for l, v  in s.items():
-            print("\t%s %s:%d" % (mod,l,v))
-    print()
-    print("\t%d exceptions" % exceptions)
-    for l,v in stats.items():
-        if l=='quiet': continue
-        print("\t%d %s messages" % (v,l))
-    print("nobodies")
-    for l,v in sorted(nobody.items(),key=lambda x: x[1], reverse=True):
-        print("\t%4d %s" % (v,l))
+# print stats
+print("stats")
+for mod, s in changes.items():
+    for l, v  in s.items():
+        print("\t%s %s:%d" % (mod,l,v))
+print()
+print("\t%d exceptions" % exceptions)
+for l,v in stats.items():
+    if l=='quiet': continue
+    print("\t%d %s messages" % (v,l))
+print("nobodies")
+for l,v in sorted(nobody.items(),key=lambda x: x[1], reverse=True):
+    print("\t%4d %s" % (v,l))
