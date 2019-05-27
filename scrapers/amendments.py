6b16caa221a871ac467e96d76350cd99a541ed54
ParlTrack <parltrack@ctrlc.hu>
2019-05-27 13:35:39 +0000

    [mod] only scrape AMCO not RPCD/OPCD, cache urls in amendments crawler

--- scrapers/amendments.py
+++ scrapers/amendments.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #    This file is part of parltrack.
 
@@ -20,6 +20,7 @@
 from utils.utils import fetch, junws
 from utils.log import log
 from utils.mappings import COMMITTEE_MAP
+from config import CURRENT_TERM
 
 CONFIG = {
     'threads': 8,
@@ -42,55 +43,58 @@ skipurls=['http://www.europarl.europa.eu/sides/getDoc.do?pubRef=-%2f%2fEP%2f%2fN
           'http://www.europarl.europa.eu/sides/getDoc.do?pubRef=-%2f%2fEP%2f%2fNONSGML%2bCOMPARL%2bPE-469.723%2b03%2bDOC%2bPDF%2bV0%2f%2fEN']
 
 def getComAms(term, update=False):
+    seen = set()
     urltpl="http://www.europarl.europa.eu/committees/en/%s/search-in-documents.html"
-    for doctype in ['AMCO', 'RPCD', 'OPCD']:
-        postdata="clean=false&leg=%s&docType=%s&miType=text&tabActif=tabResult" % (term, doctype)
-        nexttpl="action=%s"
-        for com in (k for k in COMMITTEE_MAP.keys()
-                    if len(k)==4 and k not in ['CODE', 'RETT', 'CLIM', 'TDIP', 'SURE', 'CRIM', 'CRIS']):
-            url=urltpl % (com)
-            i=0
-            log(3,'%s crawling %s, term: %s' % (doctype, com, term))
-            root=fetch(url, params=postdata)
-            prev=[]
-            while True:
-                log(3, "page %s of %s" % (i, url))
-                tmp=[]
-                for a in root.xpath('//a[@title="open this PDF in a new window"]'):
-                    u=a.get('href','')
-                    if (len(u)<=13):
-                        log(2,'url is too short, skipping: "%s"' % u)
-                        continue
-                    if u in skipurls or not u.endswith('EN'):
-                        log(3,"skipping url: %s" % u)
-                        continue
-                    rs = a.xpath('../../../p[@class="rapporteurs clearfix"]')
-                    if len(rs)!=1:
-                        log(1,"len(rs)==%d!=1 in %s" % (len(rs), url))
-                        raise ValueError("HTML strangeness in %s" % url)
-                    r = junws(rs[0])
-                    tmp.append(u)
-                    try:
-                        add_job('amendment', payload={'url':u,'meps':r})
-                    except:
-                        print(u, r)
+    #for doctype in ['AMCO', 'RPCD', 'OPCD']:
+    doctype = 'AMCO'
+    postdata="clean=false&leg=%s&docType=%s&miType=text&tabActif=tabResult" % (term, doctype)
+    nexttpl="action=%s"
+    for com in (k for k in COMMITTEE_MAP.keys()
+                if len(k)==4 and k not in ['CODE', 'RETT', 'CLIM', 'TDIP', 'SURE', 'CRIM', 'CRIS']):
+        url=urltpl % (com)
+        i=0
+        log(3,'%s crawling %s, term: %s' % (doctype, com, term))
+        root=fetch(url, params=postdata)
+        prev=[]
+        while True:
+            log(3, "page %s of %s" % (i, url))
+            tmp=[]
+            for a in root.xpath('//a[@title="open this PDF in a new window"]'):
+                u=a.get('href','')
+                if (len(u)<=13):
+                    log(2,'url is too short, skipping: "%s"' % u)
+                    continue
+                if u in seen or u in skipurls or not u.endswith('EN'):
+                    log(3,"skipping url: %s" % u)
+                    continue
+                seen.add(u)
+                rs = a.xpath('../../../p[@class="rapporteurs clearfix"]')
+                if len(rs)!=1:
+                    log(1,"len(rs)==%d!=1 in %s" % (len(rs), url))
+                    raise ValueError("HTML strangeness in %s" % url)
+                r = junws(rs[0])
+                tmp.append(u)
+                try:
+                    add_job('amendment', payload={'url':u,'meps':r})
+                except:
+                    print(u, r)
 
-                if not tmp or prev==tmp:
-                    break
-                prev=tmp
+            if not tmp or prev==tmp:
+                break
+            prev=tmp
 
-                if update: break
+            if update: break
 
-                i+=1
-                root=fetch(url, params="%s&%s" % (postdata, nexttpl % i))
+            i+=1
+            root=fetch(url, params="%s&%s" % (postdata, nexttpl % i))
 
 def scrape(all=False):
     if all:
-        for term in range(1,9):
+        # todo enable also 6th term
+        for term in range(7,CURRENT_TERM+1):
             getComAms(term)
     else:
-        # todo find out current term
-        getComAms(8, update=True)
+        getComAms(CURRENT_TERM, update=True)
 
 if __name__ == "__main__":
     scrape(all=False)
