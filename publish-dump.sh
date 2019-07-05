a5f8344d77db0604518e0ec417775d1b0e03dc45
ParlTrack <parltrack@ctrlc.hu>
2019-07-05 18:34:22 +0200

    [fix] publish-dump had ext missing

--- publish-dump.sh
+++ publish-dump.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 lzip -c -9 db/$1 >/var/www/parltrack/dumps/$1.lz-new
-mv /var/www/parltrack/dumps/$1.lz /var/www/parltrack/dumps/arch/${1%%.json}-$(date -r /var/www/parltrack/dumps/$1.lz -Idate).json
+mv /var/www/parltrack/dumps/$1.lz /var/www/parltrack/dumps/arch/${1%%.json}-$(date -r /var/www/parltrack/dumps/$1.lz -Idate).json.lz
 mv /var/www/parltrack/dumps/$1.lz-new /var/www/parltrack/dumps/$1.lz
