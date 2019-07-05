a87d1d28b7b9747591528a21172167d0ecbd5d8f
ParlTrack <parltrack@ctrlc.hu>
2019-07-05 18:33:56 +0200

    [mod] removed fetch.sh from db/

--- db/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh 
-
-for a in ep_dossiers ep_meps_current ep_comagendas ep_votes ep_amendments ep_com_votes; do
-   curl -L http://parltrack.euwiki.org/dumps/$a.json.xz | xz -dc >$a.json
-done
-mv ep_meps_current.json ep_meps.json
-
-echo "do not forget to run uniq.py on ep_dossiers!"
