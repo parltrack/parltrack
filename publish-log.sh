2562eb069096fbe8caeb67fa5c0f17f2dd93fae9
ParlTrack <parltrack@ctrlc.hu>
2019-07-06 18:54:11 +0200

    [enh] added lf.py html summary to publish-logs

--- publish-log.sh
+++ publish-log.sh
@@ -2,4 +2,5 @@
 
 today=$(date -Idate)
 cp /tmp/${today}.log /var/www/parltrack/logs/${today}.log
+./lf.py html warn < /var/www/parltrack/logs/${today}.log >/var/www/parltrack/logs/${today}.html
 lzip -9 /var/www/parltrack/logs/${today}.log
