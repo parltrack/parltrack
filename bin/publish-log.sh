ef02eb06eaea985fe6f95da7fe57026c718bca82
ParlTrack <parltrack@ctrlc.hu>
2019-07-07 22:26:27 +0200

    [mod] move not copy logs in publish-logs

--- bin/publish-log.sh
+++ bin/publish-log.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 today=$(date -Idate)
-cp /tmp/${today}.log /var/www/parltrack/logs/${today}.log
+mv /tmp/${today}.log /var/www/parltrack/logs/${today}.log
 ./lf.py html warn < /var/www/parltrack/logs/${today}.log >/var/www/parltrack/logs/${today}.html
 lzip -9 /var/www/parltrack/logs/${today}.log
