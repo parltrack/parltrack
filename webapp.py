63e41d860bf3475d18388b1fc98b6496128065af
ParlTrack <parltrack@ctrlc.hu>
2019-07-13 18:28:06 +0200

    [fix] sorting of amendments in webapp failed for alphabetic seq ids

--- webapp.py
+++ webapp.py
@@ -165,7 +165,8 @@ def dumps():
 
 
 @app.route('/log/<string:date>')
-def logs(date):
+@app.route('/log')
+def logs(date=date.today().strftime("%Y-%m-%d")):
     lf = {f for f in os.listdir("/var/www/parltrack/logs") if f.endswith('.html')}
     if ('%s.html' % date) not in lf:
         print(date, lf)
@@ -368,7 +369,7 @@ def activities(mep_id, t, d_id):
         if d_id is not None:
             ams = [a for a in ams if a['reference']==d_id]
         if ams:
-            a['amendments'] = sorted(ams, key=lambda x: (x['reference'], -x['seq'] if isinstance(x['seq'], int) else x['seq']), reverse=True)
+            a['amendments'] = sorted(ams, key=lambda x: (x['reference'], -(x['seq'] if isinstance(x['seq'], int) else ord(x['seq'][0]))), reverse=True)
 
     for k in ['mep_id', 'changes', 'meta']:
         if k in a: del a[k]
