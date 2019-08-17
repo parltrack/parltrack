6650e69dcc0f5c857dde1018a05307711bb91a60
ParlTrack <parltrack@ctrlc.hu>
2019-08-17 23:31:31 +0200

    [enh] significantly improved notification group view

--- notification_model.py
+++ notification_model.py
@@ -35,10 +35,12 @@ Base.query = session.query_property()
 class Subscriber(Base):
     __tablename__ = 'subscriber'
 
-    email = Column(String(64), unique=True, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(64))
     activation_key = Column(String(64))
+    deactivation_key = Column(String(64))
     group_id = Column(Integer, ForeignKey("group.id"))
-    group = relationship('Group', foreign_keys=[group_id], backref="subscribers", cascade="all, delete-orphan", single_parent=True)
+    group = relationship('Group', foreign_keys=[group_id], backref="subscribers", single_parent=True)
     added = Column(Date, default=date.today)
 
     def __unicode__(self):
@@ -51,11 +53,13 @@ class Subscriber(Base):
 class Item(Base):
     __tablename__ = 'item'
 
-    name = Column(String(64), unique=True, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64))
     type = Column(String(64))
     activation_key = Column(String(64))
+    deactivation_key = Column(String(64))
     group_id = Column(Integer, ForeignKey("group.id"))
-    group = relationship('Group', foreign_keys=[group_id], backref="items", cascade="all, delete-orphan", single_parent=True)
+    group = relationship('Group', foreign_keys=[group_id], backref="items", single_parent=True)
     added = Column(Date, default=date.today)
 
 
@@ -72,6 +76,7 @@ class Group(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(64), unique=True)
     activation_key = Column(String(64))
+    created = Column(Date, default=date.today)
 
     def __unicode__(self):
         return '%s (%s) - subscribers: {%s} items {%s}' % (
@@ -86,10 +91,59 @@ class Group(Base):
 
 
 if __name__ == '__main__':
-    print("Creating database")
-    try:
-        Base.metadata.create_all(engine, checkfirst=True)
-        print("Database created")
-        from IPython import embed; embed()
-    except Exception as e:
-        print("[E] Failed to create database: {0}".format(e))
+    from sys import argv, exit
+    if len(argv) == 1:
+        print('Invalid action (init/migrate/cleanup)')
+        exit(1)
+
+    if argv[1] == 'migrate':
+        print("Creating database")
+        try:
+            Base.metadata.create_all(engine, checkfirst=True)
+            print("Database created")
+            from IPython import embed; embed()
+        except Exception as e:
+            print("[E] Failed to create database: {0}".format(e))
+    elif argv[1] == 'migrate':
+        infile = argv[2]
+        print("Migrating", infile)
+        from json import load
+        with open(infile, 'rb') as of:
+            data = load(of)
+            for item in data:
+                if not item['active_emails'] or not item['dossiers']:
+                    continue
+
+                print('Migrating', item['id'], item['active_emails'], item['dossiers'])
+
+                group = session.query(Group).filter(Group.name==item['id']).first() or Group(name=item['id'])
+
+                for mail in item['active_emails']:
+                    s = Subscriber(email)
+                    session.add(s)
+                    session.commit()
+                    group.subscribers.append(s)
+
+                for d in item['dossiers']:
+                    i = Item(name=d, type='dossier')
+                    session.add(i)
+                    session.commit()
+                    group.items.append(s)
+
+                session.add(group)
+                session.commit()
+    elif argv[1] == 'cleanup':
+        from datetime import datetime, timedelta
+        for i in  session.query(Subscriber).filter(Subscriber.activation_key!='').filter(Subscriber.added<datetime.now()-timedelta(weeks=1)).all():
+            session.delete(i)
+            session.commit()
+        for i in  session.query(Item).filter(Item.activation_key!='').filter(Item.added<datetime.now()-timedelta(weeks=1)).all():
+            session.delete(i)
+            session.commit()
+        for i in  session.query(Group).filter(Group.activation_key!='').filter(Group.created<datettme.now()-timedelta(weeks=1)).all():
+            if i.subscribers or i.items:
+                continue
+            session.delete(i)
+            session.commit()
+    else:
+        print('unknown action')
--- static/js/view_notif.js
+++ static/js/view_notif.js
@@ -1,5 +1,5 @@
-var submit = function() {
-   var group_id = $('#group_id').val()
+var unsubscribe = function() {
+   var group_id = $('#group_id').val();
    $.ajax({url: group_id, success: function(data) {
       if($('#email_name').val()) $.ajax({url: group_id+'/del/emails/'+$('#email_name').val()});
       if($('#dossier_name').val()) $.ajax({url: group_id+'/del/dossiers/'+$('#dossier_name').val()});
@@ -7,17 +7,63 @@ var submit = function() {
    }
  });
  return false;
-}
+};
+var show_message = function(error) {
+   var id = '#subscribe-message';
+   if(error=='error') {
+      id = '#subscribe-error';
+   }
+   $(id).show();
+   setTimeout(function() {
+      $(id).hide();
+   }, 5000);
+};
+var submit_email = function() {
+   var group_id = $('#group_id').val();
+   var email = $('#subscribe-email').val();
+   if(email) {
+      $.ajax({url: group_id+'/add/emails/'+email, success: show_message, error: function() {show_message('error')}});
+   }
+   $('#subscribe_email').val('');
+   return false;
+};
+var submit_meps_by_country = function() {
+   var group_id = $('#group_id').val();
+   var email = $('#subscribe-meps-by-country').val();
+   if(email) {
+      $.ajax({url: group_id+'/add/meps_by_country/'+email, success: show_message, error: function() {show_message('error')}});
+   }
+   return false;
+};
+var submit_meps_by_committee = function() {
+   var group_id = $('#group_id').val();
+   var email = $('#subscribe-meps-by-committee').val();
+   if(email) {
+      $.ajax({url: group_id+'/add/meps_by_committee/'+email, success: show_message, error: function() {show_message('error')}});
+   }
+   return false;
+};
+var submit_meps_by_group = function() {
+   var group_id = $('#group_id').val();
+   var email = $('#subscribe-meps-by-group').val();
+   if(email) {
+      $.ajax({url: group_id+'/add/meps_by_group/'+email, success: show_message, error: function() {show_message('error')}});
+   }
+   return false;
+};
 
 $(document).ready(function() {
   $('#unsubscribe_link').click(function() {
      $('.modal').modal('show');
   });
-  $('#submit').click(function() {
-      submit();
-  });
-  $('#notif_form').submit(submit);
   $('#hide').click(function() {
     $('.modal').modal('hide');
   });
+  $('#submit').click(unsubscribe);
+  $('#notif_form').submit(unsubscribe);
+  $('#subscribe-meps-by-country-button').click(submit_meps_by_country);
+  $('#subscribe-meps-by-committee-button').click(submit_meps_by_committee);
+  $('#subscribe-meps-by-group-button').click(submit_meps_by_group);
+  $('#subscribe-email-button').click(submit_email);
+  $('.ui.dropdown').dropdown({clearable: true});
 });
