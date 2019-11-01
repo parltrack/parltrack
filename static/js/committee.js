374cc1b5b6d2747f42d8f23cf25871f7454b8976
ParlTrack <parltrack@ctrlc.hu>
2019-11-02 00:45:37 +0100

    [mod] made earlier agendas accordions

--- static/js/committee.js
+++ static/js/committee.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $("#tabs .item").tab({history:true, historyType: 'hash'});
   filter('meplist');
   filter('dossierlist');
+  $('.ui.accordion').accordion({
+      clearable: true,
+      placeholder: 'any'
+  }) ;
 });
