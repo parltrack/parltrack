7dc429cd5f7444a44be001d29c813668bc01f165
ParlTrack <parltrack@ctrlc.hu>
2020-05-19 17:12:42 +0200

    [mod] adapted to new interface at EP website

--- scrapers/comagendas.py
+++ scrapers/comagendas.py
@@ -15,7 +15,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with parltrack.  If not, see <http://www.gnu.org/licenses/>.
 
-# (C) 2011 by Stefan Marsiske, <parltrack@ctrlc.hu>, Asciimoo
+# (C) 2011, 2019, 2020 by Stefan Marsiske, <parltrack@ctrlc.hu>, Asciimoo
 
 from utils.utils import fetch, unws
 from utils.log import log
@@ -30,58 +30,57 @@ CONFIG = {
     'error_handler': None,
 }
 
+itemsPerPage=30
 
-def getnullq(term):
-    url="http://www.europarl.europa.eu/committees/en/search-in-documents.html"
-    postdata="docType=AGEN&leg=%s&miType=text&tabActif=tabResult#sidesForm" % term
-    res = []
-    i=0
-    log(4, 'initializing com agenda null query for term %d' % term)
-    root=fetch(url, params=postdata)
-    tmp=[(a.get('href'), unws(a.xpath('text()')[0]))
-         for a in root.xpath('//p[@class="title"]/a')
-         if len(a.get('href',''))>13]
-    return [u for u,title in tmp if title.startswith('DRAFT AGENDA')]
 
-def crawl(term, **kwargs):
-    url="https://www.europarl.europa.eu/committees/en/%s/search-in-documents.html"
-    postdata="documentDateStart=&meetingDateEnd=&documentType=&docType=AGEN&folderComCode=&refAYear=&refProcYear=&folderId=&documentDateEnd=&author=&clean=false&tabActif=tabResult&real_form_name=sidesForm&meetingDateStart=&folderLegId=&refPe=&refProcCode=&miType=text&refProcNum=&refANum=&miText=&leg=%s&committee=%s&source=&action=%s"
-    # clean=false&leg=8&action=0&tabActif=tabResult&committee=2863&docType=AGEN&author=&refPe=&refANum=&refAYear=&miType=text&miText=&documentType=&documentDateStart=&documentDateEnd=&meetingDateStart=&meetingDateEnd=&folderComCode=&folderLegId=&folderId=&refProcYear=&refProcNum=&refProcCode=
-    nullq = getnullq(term)
+def crawl(term, test=[], **kwargs):
+    seen = set()
+    url="https://www.europarl.europa.eu/committees/en/documents/search?committeeMnemoCode=%s&textualSearchMode=TITLE&textualSearch=&documentTypeCode=AGEN&reporterPersId=&procedureYear=&procedureNum=&procedureCodeType=&peNumber=&aNumber=&aNumberYear=&documentDateFrom=&documentDateTo=&meetingDateFrom=&meetingDateTo=&performSearch=true&term=%s&page=%s&pageSize={}".format(itemsPerPage)
     jobs = []
-    for com in (k for k in COMMITTEE_MAP.keys() if len(k)==4):
+    for com in (k for k in test or COMMITTEE_MAP.keys() if len(k)==4):
         i=0
         log(3,'crawling %s, term: %s' % (com, term))
         try:
-            root=fetch(url % com, params=(postdata % (term, com, i)))
+            root=fetch(url % (com, term, i))
         except requests.exceptions.HTTPError as e:
-            if e.response.status_code == 500:
-                log(3, "failed to get list of draft agendas for %s in term %d" % (com, term))
-                continue
+            #if e.response.status_code == 500:
+            log(3, "failed to get list of draft agendas for %s in term %d, http error code: %s" % (com, term, e.response.status_code))
+            continue
         prev=[]
         while True:
             log(3, "crawling comagenda search page %s for %s term %s" % (i, com, term))
-            tmp=[(a.get('href'), unws(a.xpath('text()')[0]))
-                 for a in root.xpath('//p[@class="title"]/a')
-                 if len(a.get('href',''))>13]
-            if not tmp or prev==tmp:
+            tmp=[]
+            for a in root.xpath('//a[@class="erpl_document-subtitle-pdf"]'):
+                u=a.get('href','')
+                if (len(u)<=13):
+                    log(2,'url is too short, skipping: "%s"' % u)
+                    continue
+                if u in seen or (not u.endswith('EN') and not u.endswith('_EN.pdf')):
+                    log(3,"skipping url: %s" % repr(u))
+                    continue
+                seen.add(u)
+                tmp.append(u)
+                try:
+                    payload = dict(kwargs)
+                    payload['url'] = u
+                    payload['committee']= com
+                    if test:
+                        print(payload)
+                    else:
+                        add_job('comagenda', payload=payload)
+                except:
+                    print(u, r)
+
+            if not tmp or prev==tmp or len(tmp) < itemsPerPage:
                 break
             prev=tmp
-            tmp =[u for u,title in tmp if title.startswith('DRAFT AGENDA')]
-            if tmp == nullq:
-                log(4,"no agenda items found for %s in term %d" % (com, term))
-                break
-            for u in tmp:
-                jobs.append({'url':u,'committee': com})
             i+=1
             try:
-                root=fetch(url % com, params=(postdata % (term, com, i)))
+                root=fetch(url % (com, term, i))
             except requests.exceptions.HTTPError as e:
                 if e.response.status_code == 500:
                     log(3, "failed to page %s of draft agendas for %s in term %d" % (i, com, term))
                 break
-    for job in jobs:
-        add_job('comagenda', payload=job)
 
 def scrape(all=False, **kwargs):
     if all:
@@ -91,4 +90,8 @@ def scrape(all=False, **kwargs):
         crawl(CURRENT_TERM)
 
 if __name__ == "__main__":
-    scrape(all=True)
+    import sys
+    if len(sys.argv) == 2:
+        crawl(9, test=[sys.argv[1]])
+    else:
+        scrape(all=True)
