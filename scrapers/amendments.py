d426e78b895411819c7a39cf7fc94c97885e78a1
ParlTrack <parltrack@ctrlc.hu>
2020-05-19 16:38:52 +0200

    [mod] adapted to new interface at EP website

--- scrapers/amendments.py
+++ scrapers/amendments.py
@@ -30,6 +30,8 @@ CONFIG = {
     'error_handler': None,
 }
 
+itemsPerPage=30
+
 skipurls=['http://www.europarl.europa.eu/sides/getDoc.do?pubRef=-%2f%2fEP%2f%2fNONSGML%2bCOMPARL%2bPE-483.680%2b02%2bDOC%2bPDF%2bV0%2f%2fEN',
           'http://www.europarl.europa.eu/sides/getDoc.do?pubRef=-%2f%2fEP%2f%2fNONSGML%2bCOMPARL%2bPE-454.387%2b01%2bDOC%2bPDF%2bV0%2f%2fEN',
           'http://www.europarl.europa.eu/sides/getDoc.do?pubRef=-%2f%2fEP%2f%2fNONSGML%2bCOMPARL%2bPE-456.679%2b01%2bDOC%2bPDF%2bV0%2f%2fEN',
@@ -43,39 +45,25 @@ skipurls=['http://www.europarl.europa.eu/sides/getDoc.do?pubRef=-%2f%2fEP%2f%2fN
           'http://www.europarl.europa.eu/sides/getDoc.do?pubRef=-%2f%2fEP%2f%2fNONSGML%2bCOMPARL%2bPE-469.721%2b02%2bDOC%2bPDF%2bV0%2f%2fEN',
           'http://www.europarl.europa.eu/sides/getDoc.do?pubRef=-%2f%2fEP%2f%2fNONSGML%2bCOMPARL%2bPE-469.723%2b03%2bDOC%2bPDF%2bV0%2f%2fEN']
 
-#def getnullq(term):
-#    url="http://www.europarl.europa.eu/committees/en/search-in-documents.html"
-#    postdata="docType=AMCO&leg=%s&miType=text&tabActif=tabResult#sidesForm" % term
-#    res = []
-#    i=0
-#    log(4, 'initializing amendments null query for term %d' % term)
-#    root=fetch(url, params=postdata)
-#    tmp=[(a.get('href'), unws(a.xpath('text()')[0]))
-#         for a in root.xpath('//p[@class="title"]/a')
-#         if len(a.get('href',''))>13]
-#    return [u for u,title in tmp if title.startswith('DRAFT AGENDA')]
-
-def crawl(term, update=False, **kwargs):
+def crawl(term, update=False, test=[], **kwargs):
     seen = set()
-    url="http://www.europarl.europa.eu/committees/en/%s/search-in-documents.html"
-    postdata="documentDateStart=&meetingDateEnd=&documentType=&docType=AMCO&folderComCode=&refAYear=&refProcYear=&folderId=&documentDateEnd=&author=&clean=false&tabActif=tabResult&real_form_name=sidesForm&meetingDateStart=&folderLegId=&refPe=&refProcCode=&miType=text&refProcNum=&refANum=&miText=&leg=%s&committee=%s&source=&action=%s"
-    # clean=false&leg=8&action=0&tabActif=tabResult&committee=2863&docType=AMCO&author=&refPe=&refANum=&refAYear=&miType=text&miText=&documentType=&documentDateStart=&documentDateEnd=&meetingDateStart=&meetingDateEnd=&folderComCode=&folderLegId=&folderId=&refProcYear=&refProcNum=&refProcCode=
-    #nullq = getnullq(term)
+    url="https://www.europarl.europa.eu/committees/en/documents/search?committeeMnemoCode=%s&textualSearchMode=TITLE&textualSearch=&documentTypeCode=AMCO&reporterPersId=&procedureYear=&procedureNum=&procedureCodeType=&peNumber=&aNumber=&aNumberYear=&documentDateFrom=&documentDateTo=&meetingDateFrom=&meetingDateTo=&performSearch=true&term=%s&page=%s&pageSize={}".format(itemsPerPage)
     jobs = []
-    for com in (k for k in COMMITTEE_MAP.keys() if len(k)==4):
+    for com in (k for k in test or COMMITTEE_MAP.keys() if len(k)==4):
         i=0
         log(3,'crawling %s, term: %s' % (com, term))
         try:
-            root=fetch(url % com, params=(postdata % (term, com, i)))
+            root=fetch(url % (com, term, i))
         except requests.exceptions.HTTPError as e:
-            if e.response.status_code == 500:
-                log(3, "failed to get list of amendments for %s in term %d" % (com, term))
-                continue
+            #if e.response.status_code == 500:
+            log(3, "failed to get list of amendments for %s in term %d, http error code: %s" % (com, term, e.response.status_code))
+            continue
         prev=[]
         while True:
             log(3, "crawling amendments search page %s for %s term %s" % (i, com, term))
             tmp=[]
-            for a in root.xpath('//a[@title="open this PDF in a new window"]'):
+            #for a in root.xpath('//a[@title="open this PDF in a new window"]'):
+            for a in root.xpath('//a[@class="erpl_document-subtitle-pdf"]'):
                 u=a.get('href','')
                 if (len(u)<=13):
                     log(2,'url is too short, skipping: "%s"' % u)
@@ -84,22 +72,21 @@ def crawl(term, update=False, **kwargs):
                     log(3,"skipping url: %s" % repr(u))
                     continue
                 seen.add(u)
-                rs = a.xpath('../../../p[@class="rapporteurs clearfix"]')
-                if len(rs)!=1:
-                    log(1,"len(rs)==%d!=1 in %s" % (len(rs), url))
-                    raise ValueError("HTML strangeness in %s" % url)
-                r = junws(rs[0])
                 tmp.append(u)
+                rs = a.xpath('../../following-sibling::div/span[@class="erpl_document-subtitle-author"]')
+                r = [y for y in [junws(x) for x in rs] if y]
                 try:
                     payload = dict(kwargs)
                     payload['url'] = u
                     payload['meps'] = r
-                    #print(payload)
-                    add_job('amendment', payload=payload)
+                    if test:
+                        print(payload)
+                    else:
+                        add_job('amendment', payload=payload)
                 except:
                     print(u, r)
 
-            if not tmp or prev==tmp:
+            if not tmp or prev==tmp or len(tmp) < itemsPerPage:
                 break
             prev=tmp
 
@@ -107,7 +94,7 @@ def crawl(term, update=False, **kwargs):
 
             i+=1
             try:
-                root=fetch(url % com, params=(postdata % (term, com, i)))
+                root=fetch(url % (com, term, i))
             except requests.exceptions.HTTPError as e:
                 if e.response.status_code == 500:
                     log(3, "failed to page %s of draft agendas for %s in term %d" % (i, com, term))
@@ -121,4 +108,8 @@ def scrape(all=False, **kwargs):
         crawl(CURRENT_TERM, update=True, **kwargs)
 
 if __name__ == "__main__":
-    scrape(all=True)
+    import sys
+    if len(sys.argv) == 2:
+        crawl(9, update=False, test=[sys.argv[1]])
+    else:
+        scrape(all=True)
