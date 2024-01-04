5de0aa8572e4149ac38c884f2b47879fbebde574
ParlTrack <parltrack@ctrlc.hu>
2024-01-04 15:56:23 +0000

    [mod] keep logs also in temp, don't move them with publish-logs

--- bin/publish-log.sh
+++ bin/publish-log.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 today=${1:-$(date -Idate)}
-mv /tmp/${today}.log /var/www/parltrack/logs/${today}.log
+cp /tmp/${today}.log /var/www/parltrack/logs/${today}.log
 ./lf.py html warn < /var/www/parltrack/logs/${today}.log >/var/www/parltrack/logs/${today}.html
 lzip -9 /var/www/parltrack/logs/${today}.log
