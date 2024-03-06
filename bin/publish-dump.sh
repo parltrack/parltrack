15e2957d22d498168ab8a8b25e1c1f60f6daf9cc
ParlTrack <parltrack@ctrlc.hu>
2024-03-06 16:30:50 +0000

    [mod] also shell utils sync now

--- bin/publish-dump.sh
+++ bin/publish-dump.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 lzip -c -9 db/$1 >/var/www/parltrack/dumps/$1.lz-new
+sync /var/www/parltrack/dumps/$1.lz-new
 [ -f /var/www/parltrack/dumps/$1.lz ] && {
-
    date=$(date -r /var/www/parltrack/dumps/$1.lz -Idate)
    mv /var/www/parltrack/dumps/$1.lz /var/www/parltrack/dumps/arch/${1%%.json}-${date}.json.lz
 }
--- bin/publish-log.sh
+++ bin/publish-log.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 today=${1:-$(date -Idate)}
+sync /tmp/${today}.log
 cp /tmp/${today}.log /var/www/parltrack/logs/${today}.log
 ./lf.py html warn < /var/www/parltrack/logs/${today}.log >/var/www/parltrack/logs/${today}.html
+sync /var/www/parltrack/logs/${today}.html
 lzip -9 /var/www/parltrack/logs/${today}.log
