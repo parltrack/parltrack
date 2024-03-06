1319e0ac892e5d4572688d87809008ba797fb62a
ParlTrack <parltrack@ctrlc.hu>
2024-03-06 03:23:11 +0000

    [mod] made bin/publish-dump.sh more robust

--- bin/publish-dump.sh
+++ bin/publish-dump.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 lzip -c -9 db/$1 >/var/www/parltrack/dumps/$1.lz-new
-mv /var/www/parltrack/dumps/$1.lz /var/www/parltrack/dumps/arch/${1%%.json}-$(date -r /var/www/parltrack/dumps/$1.lz -Idate).json.lz
+[ -f /var/www/parltrack/dumps/$1.lz ] && {
+
+   date=$(date -r /var/www/parltrack/dumps/$1.lz -Idate)
+   mv /var/www/parltrack/dumps/$1.lz /var/www/parltrack/dumps/arch/${1%%.json}-${date}.json.lz
+}
 mv /var/www/parltrack/dumps/$1.lz-new /var/www/parltrack/dumps/$1.lz
