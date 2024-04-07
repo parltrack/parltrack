b6d9d85bc39c66af6e54dd4e80b75f16fa8d0ca6
ParlTrack <parltrack@ctrlc.hu>
2024-04-07 19:14:11 +0000

    [fix] keep scraper cache seperate from webapp cache

--- webapp.py
+++ webapp.py
@@ -71,7 +71,7 @@ app.config.from_object('config')
 mail = Mail()
 mail.init_app(app)
 cache = Cache()
-cache.init_app(app, config={'CACHE_TYPE': 'filesystem', "CACHE_DIR": config.CACHE_DIR})
+cache.init_app(app, config={'CACHE_TYPE': 'filesystem', "CACHE_DIR": config.CACHE_DIR+"/webapp"})
 
 
 def get_changes(obj, path):
