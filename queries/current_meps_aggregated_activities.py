ea253566791525caede42bae2ed0e13b4ad62157
Adam Tauber <asciimoo@gmail.com>
2024-05-23 17:05:34 +0200

    [enh] add reproducible querying

---
+++ queries/current_meps_aggregated_activities.py
@@ -0,0 +1,31 @@
+
+def run(ctx):
+    from collections import defaultdict
+    res = defaultdict(lambda: defaultdict(int))
+    keys = set()
+
+    for d in ctx.iterate_json('ep_mep_activities'):
+        m = d['mep_id']
+        for ak, aa in d.items():
+            if ak in ('mep_id', 'meta', 'changes'):
+                continue
+            keys.add(ak)
+            for a in aa:
+                if a['term'] != 9:
+                    continue
+                res[m][ak] += 1
+
+    ctx.write_line('NAME, COUNTRY, GROUP, PARTY, ' + ', '.join(keys))
+
+    for mep in ctx.iterate_json('ep_meps'):
+        if mep['UserID'] not in res:
+            continue
+
+        activity = res[mep['UserID']]
+        name = mep['Name']['full']
+        group = ctx.latest(mep['Groups'])['groupid']
+        party = ctx.latest(mep['Constituencies'])['party']
+        country = ctx.latest(mep['Constituencies'])['country']
+
+        ctx.write_line(', '.join([x.replace(',', ' ') for x in [name, country, group, party, *[str(activity[k]) for k in keys]]]))
+
---
+++ queries/run.py
@@ -0,0 +1,74 @@
+import argparse
+from time import time
+from importlib import import_module
+from json import loads
+from operator import itemgetter
+from os import listdir
+from os.path import dirname, realpath
+
+DIR = dirname(realpath(__file__))
+MODULES = [x.replace('.py', '') for x in listdir(DIR) if x.endswith('py') and x != 'run.py']
+
+
+class Context():
+    def __init__(self, output, json_dir):
+        self.output = open(output, 'w')
+        self.json_dir = json_dir
+
+    def iterate_json(self, fname):
+        with open(f'{self.json_dir}/{fname}.json', 'r') as infile:
+            while line := infile.readline():
+                line = line[1:-1]
+                if not line:
+                    continue
+                obj = loads(line)
+                yield obj
+
+    def latest(self, l):
+        return list(sorted(l, key=itemgetter('end')))[-1]
+
+    def write_line(self, l):
+        self.output.write(f'{l}\n')
+
+    def close(self):
+        self.output.close()
+
+
+def module(mstr):
+    try:
+        m = import_module(f'{mstr}')
+    except:
+        raise ValueError(f'Module not found. Possible choices: {", ".join(MODULES)}')
+    return m
+
+
+def execute(args):
+    print(f'Executing query "{args.query.__name__}" - writing output to "{args.output}"')
+    ctx = Context(args.output, args.json_dir)
+    start = time()
+    args.query.run()
+    print(f'Successful query execution. Runtime: {(time()-start):.2f}s')
+    ctx.close()
+
+
+def list_queries(args):
+    print('Available queries:')
+    print('\n'.join(f' - {x}' for x in MODULES))
+
+
+def main():
+    p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    sp = p.add_subparsers(help='sub-command help', required=True)
+    r = sp.add_parser('execute', aliases=['e'], help='execute a query')
+    r.set_defaults(func=execute)
+    r.add_argument('query', type=module, help='query to execute')
+    r.add_argument('output', help='output file name')
+    r.add_argument('-j', '--json-dir', help='directory of the json data files', default='db/')
+    l = sp.add_parser('list', aliases=['l'], help='list queries')
+    l.set_defaults(func=list_queries)
+    args = p.parse_args()
+    args.func(args)
+
+
+if __name__ == '__main__':
+    main()
